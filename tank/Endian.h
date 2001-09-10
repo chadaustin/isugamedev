8f0116d68dcd49914e3545738e7dda3c91587cbe
subatomic <subatomic@84b32ba4-53c3-423c-be69-77cca6335494>
2001-09-10 15:40:56 +0000

    added image loader (TGA format)
    TGAs are pretty cool, they support 24 (RGB) or 32 bit (RGBA) color
    this loader does not support color maps, only 24 and 32 bit modes
    
    It does support compression. :)
    
    
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@46 84b32ba4-53c3-423c-be69-77cca6335494

---
+++ tank/Endian.h
@@ -0,0 +1,85 @@
+
+//////////////////////////////////////////////////////////////////////////////
+//
+//                         -=     machine endianness     =-
+//
+// Description: "routines for supporting machines with different endianness"
+//
+///////////////// <auto-copyright BEGIN do not edit this line> /////////////////
+//
+//    $RCSfile: Endian.h,v $
+//    $Date: 2001-09-10 15:40:56 $
+//    $Revision: 1.1 $
+//    Copyright (C) 1998, 1999, 2000  Kevin Meinert, kevin@vrsource.org
+//
+//    This library is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Library General Public
+//    License as published by the Free Software Foundation; either
+//    version 2 of the License, or (at your option) any later version.
+//
+//    This library is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Library General Public License for more details.
+//
+//    You should have received a copy of the GNU Library General Public
+//    License along with this library; if not, write to the Free
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+///////////////// <auto-copyright END do not edit this line> ///////////////////
+#ifndef ENDIAN_FUNCS
+#define ENDIAN_FUNCS
+
+// confused about namespaces?
+// to call these funcs, use kev::byteReverse() for example
+namespace kev
+{
+   //: Swap the bytes in any data type.
+   // Motorola and Intel store their bytes in reversed formats <BR>
+   // ex: An SGI image is native to the SGI system,            <BR>
+   // to be read on an intel machine, it's bytes need to be reversed <BR>
+   // NOTE: chars aren't affected by this since it only        <BR>
+   // affects the order of bytes, not bits.
+   template< class Type >
+   inline void  byteReverse(Type& bytes)
+   {       
+       const int size = sizeof(Type);
+       Type buf = 0;
+       int x, y;
+
+       //we want to index the bytes in the buffer
+       unsigned char* buffer = (unsigned char*) &buf;
+
+       for ( x = 0, y = size-1; 
+                  x < size;
+                  ++x, --y )
+       {
+         buffer[x] |= ((unsigned char*)&bytes)[y];
+       }
+       bytes = buf;
+   }
+
+   //: check the system for endianess
+   inline bool isLittleEndian() 
+   {
+      union 
+      {
+         short   val;
+         char    ch[sizeof(short)];
+      } un;
+
+      // initialize the memory that we'll probe
+      un.val = 256;       // 0x10
+
+      // If the 1 byte is in the low-order position (un.ch[1]), this is a
+      // little-endian system.  Otherwise, it is a big-endian system.
+      return un.ch[1] == 1;
+   }
+
+   //: check the system for endianess
+   inline bool isBigEndian()
+   {
+      return !isLittleEndian();
+   }   
+};
+#endif
---
+++ tank/TgaImporter.h
@@ -0,0 +1,77 @@
+
+//////////////////////////////////////////////////////////////////////////////
+//
+//                         -=     TgaImport class     =-
+//
+// Description: "Importer for Targa files.
+//
+///////////////// <auto-copyright BEGIN do not edit this line> /////////////////
+//
+//    $RCSfile: TgaImporter.h,v $
+//    $Date: 2001-09-10 15:40:56 $
+//    $Revision: 1.1 $
+//    Copyright (C) 1998, 1999, 2000  Kevin Meinert, kevin@vrsource.org
+//
+//    This library is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Library General Public
+//    License as published by the Free Software Foundation; either
+//    version 2 of the License, or (at your option) any later version.
+//
+//    This library is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Library General Public License for more details.
+//
+//    You should have received a copy of the GNU Library General Public
+//    License along with this library; if not, write to the Free
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+///////////////// <auto-copyright END do not edit this line> ///////////////////
+
+//////////////////////////////////////////////////////////////////////////////
+#ifndef TGA_IMPORTER_INCLUDED
+#define TGA_IMPORTER_INCLUDED
+
+#include "Image.h"
+
+//: TgaImporter
+// Load an image file into an Image object
+//
+// To use, declare an instance of this class,
+// then call operator() with the filename and Image object you wish to act on.
+// i.e:
+//   Image image;
+//   TgaImporter tga_import;
+//   tga_import( "bugs.tga", image );
+// 
+// alternately, you can call operator() this way:
+//   tga_import.operator()( "bugs.tga", image );
+//
+class TgaImporter
+{   
+public:
+   //: Default constructor
+   TgaImporter();      
+      
+   //: Destructor
+   virtual ~TgaImporter();
+      
+   //: Load the image "filename", loads into the Image data
+   //  returns true or false for successful or unsuccessful
+   virtual bool import( const char* const filename, Image& image );   
+   
+   //: a string that describes the files supported by this importer
+   virtual const char* const description() const;
+   
+private:
+   // Read, and expand, one row of RLE data from the filestream.
+   // data       - pointer to buffer to recieve uncompressed data.
+   // bufferSize - buffer size in bytes
+   // bpp        - bytes per pixel (8, 16, 24, 32)
+   // fp         - file pointer, must be an opened file, ::use fseek to position at valid start of RLE row.
+   static int ReadRLERow( unsigned char* data, const int& bufferSize, const int& bpp, FILE *fp );
+};
+
+
+
+#endif
