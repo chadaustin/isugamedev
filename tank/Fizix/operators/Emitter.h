edf76ab4d2e13dad6d357d0c9ae0069971a2f091
subatomic <subatomic@84b32ba4-53c3-423c-be69-77cca6335494>
2001-09-13 19:43:35 +0000

    added emitter operators
    broken, so i will be fixing it as needed...  feel free to get ideas from
    it though.
    
    
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@70 84b32ba4-53c3-423c-be69-77cca6335494

---
+++ tank/Fizix/operators/Emitter.h
@@ -0,0 +1,107 @@
+#ifndef EMITTERS
+#define EMITTERS
+
+class Emitter : public PsOperator
+{
+public:
+   Emitter() : mAgeOfDeath( 2 ), mRate( 0.1 ) {}
+   virtual ~Emitter() {}
+      void setEmissionRate( const float& rate ) { mRate = rate; }      
+   void setAgeOfDeath( float aod )
+   {
+      mAgeOfDeath = aod;
+   }
+
+   float mAgeOfDeath;
+   float mRate;
+};
+
+class FlameEmitter : public Emitter
+{
+public:
+   FlameEmitter() : kRandMax( RAND_MAX ), x(0), velocitizer(0), Emitter(), mSize( 8 ) { srand( 2); }
+   virtual ~FlameEmitter() {}
+   void setSize( float size ) { mSize = size; }
+   virtual void operator()( ParticleSystem& ps )
+   {
+      for (x += ps.timeDelta(); x > mRate; x -= mRate )
+      {
+         Particle* p = new Particle;
+	      p->setMass( 1.0f ); // 1 kilogram
+         float r1 = rand(), r2 = rand(), r3, r4 = rand(), r5 = rand();
+         r1 /= kRandMax;
+         r2 /= kRandMax;
+         r4 /= kRandMax;
+         r5 /= kRandMax;
+         r3 = (r1 + r2) * 0.5f;
+
+         // compute random position within the space.
+         float posx = -mSize * 0.5f + r4 * mSize;
+         float posz = -mSize * 0.5f + r5 * mSize;
+
+         // make the velocity increase in the middle.
+         float vely = 1.0f - kev::min( kev::ABS(posx / mSize), kev::ABS(posz / mSize) );
+         const float multifactor = 3.0f;
+
+         float velx = r1 * 2.0f - 1.0f;
+         float velz = r2 * 2.0f - 1.0f;
+         Vec3<float> velocity( velx, (r3 * 1.0f + 3.8f) * (vely * multifactor), velz );
+	      
+         Vec3<float> position( posx, 0.0f, posz );
+	      ColorRGBA color( 0.0f, 0.0f, 0.0f, 1.0f );
+	      p->setVelocity( velocity );
+	      p->setPosition( position );
+	      p->setColor( color );
+         p->setAgeOfDeath( mAgeOfDeath );
+	      ps.add( p );
+         p->unrefDelete(); // give up responsibility of this object.
+      }
+
+      if (x < 0) x = 0;
+   }
+
+   float x;
+   float velocitizer;
+   const float kRandMax;
+   float mSize;
+};
+
+class SpiralEmitter : public Emitter
+{
+public:
+   SpiralEmitter() : velocitizer(0), Emitter(), x(0), pos( 0,0,0 ) {}
+   virtual ~SpiralEmitter() {}
+   void setPosition( float x, float y, float z )
+   {
+      pos.set( x, y, z );
+   }         
+
+   virtual void operator() (ParticleSystem& ps )
+   {
+      for (x += ps.timeDelta(); x > mRate; x -= mRate )
+      {
+         velocitizer += 0.75f * ps.timeDelta();
+         if (velocitizer > 1.0f || velocitizer <= 0)
+            velocitizer = 0.0f;
+         Particle* p = new Particle;
+	      p->setMass( 1.0f ); // 1 kilogram
+	      Vec3<float> velocity( kev::SIN( velocitizer * TWO_PI_F ) * 5.0f, 19.6f, kev::COS( velocitizer * TWO_PI_F ) * 5.0f );
+	      ColorRGBA color( 0.3f, 0.2f, 1.0f, 1.0f );
+	      p->setVelocity( velocity );
+	      p->setPosition( pos );
+	      p->setColor( color );
+         p->setAgeOfDeath( 10 );
+	      ps.add( p );
+         p->unrefDelete(); // give up responsibility of this object.
+      }
+
+      if (x < 0) x = 0;
+   }
+
+   float x;
+   float velocitizer;
+   Vec3<float> pos;
+};
+
+
+#endif
