8f8c8cf1bb72f9b668ff3ae7f546f8d74be9b8d1
nonchocoboy <nonchocoboy@84b32ba4-53c3-423c-be69-77cca6335494>
2001-09-21 16:50:37 +0000

    enabled back face culling to fix a flicker problem on linux. this should really be enabled anyway for performance reasons.
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@130 84b32ba4-53c3-423c-be69-77cca6335494

--- tank/simpleGlutApp.cxx
+++ tank/simpleGlutApp.cxx
@@ -133,7 +133,9 @@ static void OnRedisplay()
    glClearColor( 0.0f, 0.0f, 0.0f, 1.0f );
    glClear( GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT );
    glEnable( GL_DEPTH_TEST );
-   glDisable( GL_CULL_FACE );
+   glFrontFace( GL_CCW );
+   glCullFace( GL_BACK );
+   glEnable( GL_CULL_FACE );
    glEnable( GL_BLEND );
    glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA ); 
    glEnable( GL_COLOR_MATERIAL ); // enable materials
