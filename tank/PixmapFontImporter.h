7c61d728630738e292a5c3ef465c9fefd0d6329a
subatomic <subatomic@84b32ba4-53c3-423c-be69-77cca6335494>
2001-09-27 22:51:30 +0000

    fixed a nasty bug where we write into position -4 in an std::vector
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@157 84b32ba4-53c3-423c-be69-77cca6335494

--- tank/PixmapFontImporter.h
+++ tank/PixmapFontImporter.h
@@ -8,8 +8,8 @@
 ///////////////// <auto-copyright BEGIN do not edit this line> /////////////////
 //
 //    $RCSfile: PixmapFontImporter.h,v $
-//    $Date: 2001-09-26 22:58:56 $
-//    $Revision: 1.1 $
+//    $Date: 2001-09-27 22:51:30 $
+//    $Revision: 1.2 $
 //    Copyright (C) 1998, 1999, 2000  Kevin Meinert, kevin@vrsource.org
 //
 //    This library is free software; you can redistribute it and/or
@@ -47,23 +47,26 @@ static void loadGlyphMap( const char* const filename, PixmapFont& font )
    float height = 6;
    int index = 0;
    PixmapFont::GlyphData mapping;
-   FILE* fp = ::fopen( filename, "rb" );
-   assert( fp != NULL );
-   fscanf( fp, "%f", &height );
+   
+   std::ifstream map_file( filename, std::ios::in );
+   assert( map_file.good() && "- file not found" );
+   
+   map_file>>height;
    font.setHeight( height );
    while (index != -4 )
    {
-      fscanf( fp, "%d", &index );
-      fscanf( fp, "%f", &mapping.width() );
+      map_file>>index;
+      if (index == -4) break;
+      map_file>>mapping.width();
       
-      fscanf( fp, "%f %f", &mapping.t1[0], &mapping.t1[1] );
-      fscanf( fp, "%f %f", &mapping.t2[0], &mapping.t2[1] );
-      fscanf( fp, "%f %f", &mapping.t3[0], &mapping.t3[1] );
-      fscanf( fp, "%f %f", &mapping.t4[0], &mapping.t4[1] );
+      map_file>>mapping.t1[0]>>mapping.t1[1];
+      map_file>>mapping.t2[0]>>mapping.t2[1];
+      map_file>>mapping.t3[0]>>mapping.t3[1];
+      map_file>>mapping.t4[0]>>mapping.t4[1];
       
       font.setGlyphData( index, mapping );
    }
-   fclose( fp );
+   map_file.close();
 }
 
 static void import( const char* const filename, PixmapFont& font )
@@ -73,8 +76,7 @@ static void import( const char* const filename, PixmapFont& font )
    std::ifstream fontFile( filename, std::ios::in );
    assert( fontFile.good() && "- file not found" );
    		
-   char key[256], value[256];
-   std::string file_name;
+   std::string file_name, key, value;
 
    // set the name
    font.setName( filename );
@@ -83,14 +85,14 @@ static void import( const char* const filename, PixmapFont& font )
    {
       fontFile>>key>>value;
       file_name = value;
-      if ( std::string( key ) == "map" )
+      if ( key == "map" )
       {
-         std::cout<<"- loading map file: "<<file_name.c_str()<<"\n"<<std::flush;
+         std::cout<<"- loading map file: "<<file_name<<"\n"<<std::flush;
          loadGlyphMap( file_name.c_str(), font );
       }
-      else if ( std::string( key ) == "image" )
+      else if ( key == "image" )
       {
-         std::cout<<"- loading image file: "<<file_name.c_str()<<"\n"<<std::flush;
+         std::cout<<"- loading image file: "<<file_name<<"\n"<<std::flush;
          TgaImporter ti;
          Image* image = new Image;
          ti( file_name.c_str(), *image );
