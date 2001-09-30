77556c6e772af6b13625ce23cdeee4fb67f3bafc
subatomic <subatomic@84b32ba4-53c3-423c-be69-77cca6335494>
2001-09-30 23:21:54 +0000

    backed out last change, got multiple instances under win32... badbadbad...
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@173 84b32ba4-53c3-423c-be69-77cca6335494

--- tank/Singleton.h
+++ tank/Singleton.h
@@ -8,8 +8,8 @@
 ///////////////// <auto-copyright BEGIN do not edit this line> /////////////////
 //
 //    $RCSfile: Singleton.h,v $
-//    $Date: 2001-09-28 19:00:23 $
-//    $Revision: 1.2 $
+//    $Date: 2001-09-30 23:21:54 $
+//    $Revision: 1.3 $
 //    Copyright (C) 1998, 1999, 2000  Kevin Meinert, kevin@vrsource.org
 //
 //    This library is free software; you can redistribute it and/or
@@ -44,7 +44,7 @@ namespace kev
       // access your class with myClass::instance().
       inline static singleClass& instance( void )        
       {    
-         /*                                             
+                                                      
          static singleClass* the_instance1 = NULL;   
 
          if (the_instance1 == NULL)
@@ -53,9 +53,9 @@ namespace kev
             { the_instance1 = new singleClass; }
          }                                      
          return *the_instance1;    
-         */
-         static singleClass the_instance1;
-         return the_instance1;
+         
+         //static singleClass the_instance1;
+         //return the_instance1;
       }
 
    protected:
