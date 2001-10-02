3cefa27975e9c9d19789642e80dd292b18f8c923
nonchocoboy <nonchocoboy@84b32ba4-53c3-423c-be69-77cca6335494>
2001-10-02 20:38:46 +0000

    we might need this file to compile ...
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@188 84b32ba4-53c3-423c-be69-77cca6335494

---
+++ tank/glRenderGeode.h
@@ -0,0 +1,18 @@
+#ifndef GLRENDERGEODE_H_INCLUDED
+#define GLRENDERGEODE_H_INCLUDED
+
+#include <assert.h>
+#include "Geode.h"
+#include "glRenderGeoSet.h"
+
+namespace kev
+{
+   //: Renders the given geode and all of its container geosets.
+   inline void glRenderGeode( const Geode *geode )
+   {
+      assert( geode != NULL && "glRenderGeode was given a NULL geode ptr" );
+      glRenderGeoSets( geode->getGeoSets() );
+   }
+}
+
+#endif // ! GLRENDERGEODE_H_INCLUDED
