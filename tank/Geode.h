8861ba9ef1f85e6f93d322181c6b61e5382ce04c
nonchocoboy <nonchocoboy@84b32ba4-53c3-423c-be69-77cca6335494>
2001-10-03 18:03:05 +0000

    Geodes now have a name for easier debugging.
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@202 84b32ba4-53c3-423c-be69-77cca6335494

--- tank/Geode.h
+++ tank/Geode.h
@@ -1,12 +1,16 @@
 #ifndef GEODE_H_INCLUDED
 #define GEODE_H_INCLUDED
 
+#include <string>
 #include "GeoSet.h"
 
 //: Defines a collection of GeoSets.
 class Geode : public RefObj
 {
 public:
+   //: Creates a new geode containing zero geosets with the name "".
+   Geode();
+
    //: Adds the given geoset to this geode. The geoset is added to the end of
    //  the list of geosets. The geoset will be deleted when this geode is
    //  destroyed unless you maintained a safe_ptr to that geoset.
@@ -15,6 +19,9 @@ public:
    //: Removes the given geoset from this geode. If the geoset does not already
    //  contain the given geoset, no work is done.
    void remove( const GeoSet *gset );
+
+   //: Removes all geosets from this geode.
+   void removeAll();
 
    //: Returns the number of geosets in this geode.
    int getNumGeoSets() const;
@@ -26,7 +33,14 @@ public:
    //: Returns the list of all geosets in this geode.
    const std::vector< safe_ptr<GeoSet> > getGeoSets() const;
 
+   //: Gets the name of this geode.
+   std::string getName() const;
+
+   //: Sets the name of this geode.
+   void setName( const std::string &name );
+
 private:
+   std::string mName;
    std::vector< safe_ptr<GeoSet> > mGeoSets;
 };
 
