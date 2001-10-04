8173e3697ed8503f834cff3d9f1bfbea874e069c
viperscum <viperscum@84b32ba4-53c3-423c-be69-77cca6335494>
2001-10-04 16:38:07 +0000

    collision should work with this algorithm...but it still doesn't...I think this part is correct though
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@207 84b32ba4-53c3-423c-be69-77cca6335494

--- examples/tankwars/collide.cpp
+++ examples/tankwars/collide.cpp
@@ -1,20 +1,22 @@
 #include "collide.h"
 
-bool ifNCollide(generic obj1, generic obj2)
+bool ifNCollide(generic *obj1, generic *obj2)
 {
 	bool nCollide = true;
 
-	GLfloat obj1X = obj1.getX();
-	GLfloat obj1Z = obj1.getZ();
-	GLfloat obj1Extents = obj1.getExtents();
+	GLfloat obj1X = obj1->getX();
+	GLfloat obj1Z = obj1->getZ();
+	GLfloat obj1Extents = obj1->getExtents();
 
-	GLfloat obj2X = obj2.getX();
-	GLfloat obj2Z = obj2.getZ();
-	GLfloat obj2Extents = obj2.getExtents();
+	GLfloat obj2X = obj2->getX();
+	GLfloat obj2Z = obj2->getZ();
+	GLfloat obj2Extents = obj2->getExtents();
 
+	GLfloat maxLength = obj1Extents + obj2Extents;
+	GLfloat lengthBetween = sqrt(((obj1X + obj2X)*(obj1X + obj2X)) + ((obj1Z + obj2Z)*(obj1Z + obj2Z)));
 
-
-
+	if ( lengthBetween <= maxLength)
+		nCollide = false;
 
 	return nCollide;
 }
--- examples/tankwars/collide.h
+++ examples/tankwars/collide.h
@@ -1,6 +1,6 @@
 #include "genInclude.h"
 
 
-bool ifNCollide(const generic& obj1, const generic& obj2);
+bool ifNCollide(generic *obj1, generic *obj2);
 //	returns true if obj1 is NOT colliding with obj2
 //  takes in pointers to 2 objects
