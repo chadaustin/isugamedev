628840f01fa3bd1a858b217ac97bc10af3ccae3b
subatomic <subatomic@84b32ba4-53c3-423c-be69-77cca6335494>
2001-10-12 04:45:22 +0000

    use const where appropriate
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@271 84b32ba4-53c3-423c-be69-77cca6335494

--- tank/Fizix/example/cubeGeometry.h
+++ tank/Fizix/example/cubeGeometry.h
@@ -6,12 +6,13 @@ class cubeGeometry
 public:
    // number of verticies
    // this can be passed to opengl's glDrawArrays( .., .. );
-   unsigned int size() { return cubeGeometry::mSize; }
+   unsigned int size() const { return cubeGeometry::mSize; }
    
    // format is: T2F_C4F_N3F_V3F
    float* data() { return (float*)cubeGeometry::mData; }
+   const float* data() const { return (float*)cubeGeometry::mData; }
 
-   void render()
+   void render() const
    {
       glEnable( GL_VERTEX_ARRAY );
       glInterleavedArrays( GL_T2F_C4F_N3F_V3F, 0, (float*)mData ); //GL_T2F_C4F_N3F_V3F format
