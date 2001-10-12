27835a6b4d3de887a4d43e92cb0e60bcaaca9962
nonchocoboy <nonchocoboy@84b32ba4-53c3-423c-be69-77cca6335494>
2001-10-12 22:31:54 +0000

    added ability to do a dynamic cast with shared pointers sp_dynamic_cast<>()
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@276 84b32ba4-53c3-423c-be69-77cca6335494

--- tank/boost/boost/smart_ptr.hpp
+++ tank/boost/boost/smart_ptr.hpp
@@ -270,6 +270,18 @@ template<typename T, typename U>
   inline bool operator!=(const shared_ptr<T>& a, const shared_ptr<U>& b)
     { return a.get() != b.get(); }
 
+//  we need the ability to do downcasting for polymorphic objects whose pointers
+//  are being shared. This code was pulled off the boost mailing list. See
+//  http://groups.yahoo.com/group/boost/message/12019
+template<typename T, typename U>
+inline shared_ptr<T>& sp_dynamic_cast( const shared_ptr<U>& a )
+{
+   if ( dynamic_cast<T *>(a.get()) ) {
+      return *( shared_ptr<T>* )&a;
+   }
+   return shared_ptr<T>(0);
+}
+
 //  shared_array  ------------------------------------------------------------//
 
 //  shared_array extends shared_ptr to arrays.
