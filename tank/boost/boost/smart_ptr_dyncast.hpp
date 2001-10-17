fcd66446a8d139dc16f7eb3d51e455760d44338c
nonchocoboy <nonchocoboy@84b32ba4-53c3-423c-be69-77cca6335494>
2001-10-17 03:34:12 +0000

    - removed sp_dynamic_cast from the boost namespace (too much of a pain in   the as to type boost:: in front of every time. - fixed the problem with returning a reference to a local variable.
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@284 84b32ba4-53c3-423c-be69-77cca6335494

--- tank/boost/boost/smart_ptr_dyncast.hpp
+++ tank/boost/boost/smart_ptr_dyncast.hpp
@@ -4,21 +4,16 @@
 #define SMART_PTR_DYNCAST_ADDON_TO_BOOST_
 
 
-namespace boost
-{
-
 //  we need the ability to do downcasting for polymorphic objects whose pointers
 //  are being shared. This code was pulled off the boost mailing list. See
 //  http://groups.yahoo.com/group/boost/message/12019
 template<typename T, typename U>
-inline shared_ptr<T>& sp_dynamic_cast( const shared_ptr<U>& a )
+inline boost::shared_ptr<T> sp_dynamic_cast( const boost::shared_ptr<U>& a )
 {
    if ( dynamic_cast<T *>(a.get()) ) {
-      return *( shared_ptr<T>* )&a;
+      return *( boost::shared_ptr<T>* )&a;
    }
-   return shared_ptr<T>(0);
-}
-
+   return boost::shared_ptr<T>(0);
 }
 
 #endif
