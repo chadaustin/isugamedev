caab55448f559df5d4f466f91d2599322300ef8c
subatomic <subatomic@84b32ba4-53c3-423c-be69-77cca6335494>
2002-01-30 16:32:04 +0000

    added the autocopyright script to the gamekernel files.
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@421 84b32ba4-53c3-423c-be69-77cca6335494

---
+++ aki/tools/gk-heading-replace.c.pl
@@ -0,0 +1,5 @@
+$begintag = '/////////////////// <GK heading BEGIN do not edit this line> /////////////////';
+$endtag = '////////////////// <GK heading END do not edit this line> ///////////////////';
+
+$newbegintag = "$begintag";
+$newendtag = "$endtag";
