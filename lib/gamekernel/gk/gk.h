4fe5d3a00c7deddbcf2c657375a573ff7c6d8278
nonchocoboy <nonchocoboy@84b32ba4-53c3-423c-be69-77cca6335494>
2002-02-25 21:22:37 +0000

    Added a convenience include header for client apps.
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@639 84b32ba4-53c3-423c-be69-77cca6335494

---
+++ lib/gamekernel/gk/gk.h
@@ -0,0 +1,43 @@
+
+/////////////////// <GK heading BEGIN do not edit this line> /////////////////
+//
+// gamekernel - a platform for running games
+// gamekernel is (C) Copyright 2001-2002 by Kevin Meinert, Ben Scott
+//
+// Authors: Kevin Meinert <kevin@vrsource.org>
+//          Ben Scott <bscott@iastate.edu>
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Library General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Library General Public License for more details.
+//
+// You should have received a copy of the GNU Library General Public
+// License along with this library; if not, write to the
+// Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+// Boston, MA 02111-1307, USA.
+//
+// -----------------------------------------------------------------
+// File:          $RCSfile: gk.h,v $
+// Date modified: $Date: 2002-02-25 21:22:37 $
+// Version:       $Revision: 1.1 $
+// -----------------------------------------------------------------
+//
+////////////////// <GK heading END do not edit this line> ///////////////////
+#ifndef GK_GK_H
+#define GK_GK_H
+
+// include all useful gk headers for easy use by the client program
+#include <gk/AnalogInterface.h>
+#include <gk/DigitalInterface.h>
+#include <gk/GameApp.h>
+#include <gk/GameKernel.h>
+#include <gk/GameInputConfigure.h>
+#include <gk/SystemDriverFactory.h>
+
+#endif
