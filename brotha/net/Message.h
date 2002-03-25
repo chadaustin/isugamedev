afe76ec2adc11b067dfb73b358cc77303657e547
aegis <aegis@84b32ba4-53c3-423c-be69-77cca6335494>
2002-03-25 23:35:39 +0000

    made serialize and deserialize take refs, not ptrs
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@732 84b32ba4-53c3-423c-be69-77cca6335494

--- brotha/net/Message.h
+++ brotha/net/Message.h
@@ -8,8 +8,8 @@ namespace net {
   public:
     virtual ~Message() { }
     virtual int getType() = 0;
-    virtual void serialize(IOutputStream* os) = 0;
-    virtual void deserialize(IInputStream* os) = 0;
+    virtual void serialize(OutputStream& os) = 0;
+    virtual void deserialize(InputStream& os) = 0;
   };
 
 }
