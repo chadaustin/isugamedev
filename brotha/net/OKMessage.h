8ec2e70e52b01fca6431276b1abf8e41b1d371ab
takanara <takanara@84b32ba4-53c3-423c-be69-77cca6335494>
2002-03-26 19:14:04 +0000

    oops, forgot to cvs add this file last night :P
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@741 84b32ba4-53c3-423c-be69-77cca6335494

---
+++ brotha/net/OKMessage.h
@@ -0,0 +1,41 @@
+#ifndef NET_OK_MESSAGE_H
+#define NET_OK_MESSAGE_H
+
+
+#include <string>
+#include "Message.h"
+#include "MessageTypes.h"
+#include "Serialize.h"
+
+/*ripped from Aegis's LogonMessage.h file*/
+
+
+namespace net {
+
+  class OKMessage : public Message {
+  public:
+    OKMessage() {
+
+    }
+
+    int getType() {
+      return MSG_OK;
+    }
+
+    void serialize(OutputStream& os) {
+      os << "OK";
+    }
+
+    void deserialize(InputStream& is) {
+      is >> mOK;
+	  assert mOK == "OK";
+    }
+
+  private:
+    std::string mOK;  //hehe 'mOK' :P
+  };
+
+}
+
+
+#endif
