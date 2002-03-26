d029c36b57121e6d1440b7177d5d8ae32694900d
tamc <tamc@84b32ba4-53c3-423c-be69-77cca6335494>
2002-03-26 23:32:15 +0000

    Initial Win32 API server, doesn't do anything but make the window
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@753 84b32ba4-53c3-423c-be69-77cca6335494

---
+++ brotha/server/main.cpp
@@ -0,0 +1,85 @@
+#include <windows.h>
+#include <string>
+
+#include "resource.h"
+
+LRESULT CALLBACK WndProc(HWND wnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+   static HWND wndLog;
+
+   switch(msg) {
+      case WM_CREATE: {
+         wndLog = CreateWindow("edit", NULL, WS_CHILD | //WS_DISABLED |
+               WS_VISIBLE | WS_HSCROLL | WS_VSCROLL | WS_BORDER | ES_LEFT | 
+               ES_MULTILINE | ES_AUTOHSCROLL | ES_AUTOVSCROLL, 0, 0, 0, 0,
+               wnd, (HMENU)1, ((LPCREATESTRUCT)lParam)->hInstance, NULL);
+
+         HMENU menu = GetMenu(wnd);
+         EnableMenuItem(menu, ID_SERVER_STOP, MF_GRAYED);
+         EnableMenuItem(menu, ID_WEBSERVER_STOP, MF_GRAYED);
+      } return 0;
+      case WM_SIZE:
+         MoveWindow(wndLog, 0, 0, LOWORD(lParam), HIWORD(lParam), true);
+         return 0;
+      case WM_DESTROY:
+         PostQuitMessage(0);
+         return 0;
+      case WM_COMMAND: {
+         HMENU menu = GetMenu(wnd);
+         switch(LOWORD(wParam)) {
+            case ID_SERVER_START:
+               EnableMenuItem(menu, ID_SERVER_START, MF_GRAYED);
+               EnableMenuItem(menu, ID_SERVER_STOP, MF_ENABLED);
+               break;
+            case ID_SERVER_STOP:
+               EnableMenuItem(menu, ID_SERVER_START, MF_ENABLED);
+               EnableMenuItem(menu, ID_SERVER_STOP, MF_GRAYED);
+               break;
+            case ID_SERVER_SETTINGS:
+               break;
+            case ID_WEBSERVER_START:
+               EnableMenuItem(menu, ID_WEBSERVER_START, MF_GRAYED);
+               EnableMenuItem(menu, ID_WEBSERVER_STOP, MF_ENABLED);
+               break;
+            case ID_WEBSERVER_STOP:
+               EnableMenuItem(menu, ID_WEBSERVER_START, MF_ENABLED);
+               EnableMenuItem(menu, ID_WEBSERVER_STOP, MF_GRAYED);
+               break;
+         }
+      } break;
+   }
+   return DefWindowProc(wnd, msg, wParam, lParam);
+}
+
+int WINAPI WinMain(HINSTANCE instance, HINSTANCE prevInstance, PSTR cmdLine, int cmdShow) {
+   std::string appName = "WAB Server";
+
+   WNDCLASSEX wndClass;
+   wndClass.cbSize        = sizeof(wndClass);
+   wndClass.style         = CS_HREDRAW | CS_VREDRAW;
+   wndClass.lpfnWndProc   = WndProc;
+   wndClass.cbClsExtra    = 0;
+   wndClass.cbWndExtra    = 0;
+   wndClass.hInstance     = instance;
+   wndClass.hIcon         = LoadIcon(NULL, IDI_APPLICATION);
+   wndClass.hIconSm       = LoadIcon(NULL, IDI_APPLICATION);
+   wndClass.hCursor       = LoadCursor(NULL, IDC_ARROW);
+   wndClass.hbrBackground = (HBRUSH) GetStockObject(WHITE_BRUSH);
+   wndClass.lpszMenuName  = MAKEINTRESOURCE(IDR_WAB);
+   wndClass.lpszClassName = appName.c_str();
+
+   RegisterClassEx(&wndClass);
+
+   HWND wnd = CreateWindow(appName.c_str(), appName.c_str(),
+         WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 600, 400, NULL,
+         NULL, instance, NULL);
+
+   ShowWindow(wnd, cmdShow);
+   UpdateWindow(wnd);
+
+   MSG msg;
+   while(GetMessage(&msg, NULL, 0, 0)) {
+      TranslateMessage(&msg);
+      DispatchMessage(&msg);
+   }
+   return (int)msg.wParam;
+}
---
+++ brotha/server/resource.h
@@ -0,0 +1,21 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by server.rc
+//
+#define IDR_WAB                         101
+#define ID_SERVER_START                 102
+#define ID_SERVER_STOP                  103
+#define ID_SERVER_SETTINGS              104
+#define ID_WEBSERVER_START              105
+#define ID_WEBSERVER_STOP               106
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        107
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
