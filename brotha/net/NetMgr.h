d7fbbe27a38d2e8458d37e12c83713a62d0543ef
nonchocoboy <nonchocoboy@84b32ba4-53c3-423c-be69-77cca6335494>
2002-03-28 03:53:24 +0000

    - Added comments. - Modified readAll's signature.
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@788 84b32ba4-53c3-423c-be69-77cca6335494

--- brotha/net/NetMgr.h
+++ brotha/net/NetMgr.h
@@ -1,7 +1,7 @@
 /* -*- Mode: C++; tab-width: 3; indent-tabs-mode: nil; c-basic-offset: 3 -*- */
 // vim:cindent:ts=3:et:sw=3:
-#ifndef NET_MANAGER_H
-#define NET_MANAGER_H
+#ifndef NET_NET_MANAGER_H
+#define NET_NET_MANAGER_H
 
 #include "Socket.h"
 #include "Message.h"
@@ -9,7 +9,16 @@
 #include <utility>
 
 namespace net {
+   /**
+    * Facade for all interactions over the network. All needed threads are
+    * created and managed internally as well as the serialization and
+    * deserialization of the messages.
+    */
    class NetMgr {
+   public:
+      /// The connection ID type.
+      typedef int ConnID;
+
    public:
       NetMgr() {
       }
@@ -17,19 +26,36 @@ namespace net {
       ~NetMgr() {
       }
 
+      /**
+       * Creates the resources necessary to handle the connection using the
+       * given socket.
+       *
+       * @param sock    the socket to manage
+       *
+       * @return  the ID for the created connection
+       */
       int handleSocket(Socket *socket) {
          return 0;
       }
 
-      void send(Message *msg, int destination) {
+      /**
+       * Sends the given message over the connection with the given ID.
+       *
+       * @param msg     the message to send over the wire
+       * @param dest    the destination connection ID
+       */
+      void send(Message *msg, int dest) {
+         /// @todo fill me
       }
 
-      std::vector<std::pair<Message*, int> > readAll() {
-         std::vector<std::pair<Message*, int> > retVal;
-
-         // TODO
-
-         return retVal;
+      /**
+       * Reads all messages that have been read since the last read and puts
+       * them in the given vector.
+       *
+       * @param msgs    filled with the messages read
+       */
+      void readAll( std::vector< std::pair<Message*, ConnID> >& msgs ) {
+         /// @todo fill me
       }
    };
 }
