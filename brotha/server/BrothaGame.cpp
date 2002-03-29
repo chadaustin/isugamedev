a3ab5f71a06a5c3aa0a4fb80dbb9cdf45c875545
tamc <tamc@84b32ba4-53c3-423c-be69-77cca6335494>
2002-03-29 11:06:50 +0000

    These are the files I meant to commit
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@864 84b32ba4-53c3-423c-be69-77cca6335494

---
+++ brotha/server/BrothaGame.cpp
@@ -0,0 +1,76 @@
+/************************************************************** brotha-head beg
+ *
+ * Warn-a-Brotha
+ *
+ * Original Authors:
+ *    Chad Austin <aegis@aegisknight.org>
+ *    Josh Carlson <josh@chegg.com>
+ *    Jeremy Hanson <jjhanson@iastate.edu>
+ *    Chad Okere <cokere@hatori42.com>
+ *    Ben Scott <bscott@iastate.edu>
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile: BrothaGame.cpp,v $
+ * Date modified: $Date: 2002-03-29 11:06:50 $
+ * Version:       $Revision: 1.1 $
+ * -----------------------------------------------------------------
+ *
+ *********************************************************** brotha-head-end */
+/*************************************************************** brotha-cpr beg
+ *
+ * Warn-a-Brotha
+ * Warn-a-Brotha is (C) Copyright 2002 by
+ *    Chad Austin, Josh Carlson, Jeremy Hanson, Chad Okere, Ben Scott
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ ************************************************************ brotha-cpr-end */
+#include "BrothaGame.h"
+
+namespace server {
+   BrothaGame::BrothaGame() {
+   }
+
+   void BrothaGame::update() {
+      /// @todo update the game state
+   }
+
+   void BrothaGame::add( game::Player* player, net::NetMgr::ConnID cID ) {
+      assert( player != NULL && "Cannot add a NULL player!" );
+      mConnectedPlayers[cID] = player;
+   }
+
+   void BrothaGame::joinPlayer( net::NetMgr::ConnID cID ) {
+      game::Player *player = mConnectedPlayers[cID];
+      mPlayers[player->getUID()] = player;
+   }
+
+   void BrothaGame::setPaused( bool pause ) {
+      if ( pause ) {
+         mGameTime.stop();
+      } else {
+         mGameTime.start();
+      }
+   }
+
+   bool BrothaGame::isPaused() const {
+      return ( ! mGameTime.isStopped() );
+   }
+
+   const game::GameTimer& BrothaGame::getGameTimer() const {
+      return mGameTime;
+   }
+}
--- brotha/server/BrothaGame.h
+++ brotha/server/BrothaGame.h
@@ -11,8 +11,8 @@
  *
  * -----------------------------------------------------------------
  * File:          $RCSfile: BrothaGame.h,v $
- * Date modified: $Date: 2002-03-29 11:04:43 $
- * Version:       $Revision: 1.1 $
+ * Date modified: $Date: 2002-03-29 11:06:50 $
+ * Version:       $Revision: 1.2 $
  * -----------------------------------------------------------------
  *
  *********************************************************** brotha-head-end */
@@ -46,14 +46,12 @@
 #include "game/Player.h"
 #include "net/NetMgr.h"
 
-namespace game
-{
+namespace server {
    /**
     * Contains the current state information relative to the game as well as all
     * the data required to display the game.
     */
-   class BrothaGame
-   {
+   class BrothaGame {
    public:
       /**
        * Creates a new game with no players.
@@ -66,29 +64,21 @@ namespace game
       void update();
 
       /**
-       * Adds the given player to the game.
+       * Adds the given player to players that have connection pool.
        *
        * @param player     the player to add
+       * @param cID        the connection id
        * @pre  player != NULL
        */
-      void add( Player* player);
+      void add( game::Player* player, net::NetMgr::ConnID cID);
 
       /**
-       * Sets which player is on the local machine. If player is NULL, then it is
-       * assumed that there is no local player and this is a dedicated server.
+       * Joins the player with said connection to the game.
        *
-       * @param player     the player corresponding to the local player
+       * @param cID     the connection id
+       * @pre  cID has already been added
        */
-      void setLocalPlayer( Player* player );
-
-      /**
-       * Gets the player on the local machine. If there is no local player, as
-       * could be the case on a dedicated server, this method will return NULL.
-       *
-       * @return  the local player or NULL if there is no player on the local
-       *          machine
-       */
-      Player* getLocalPlayer();
+      void joinPlayer( net::NetMgr::ConnID cID );
 
       /**
        * Pauses or unpauses the game.
@@ -107,23 +97,20 @@ namespace game
       /**
        * Gets the game timer
        */
-      const GameTimer& getGameTimer() const;
+      const game::GameTimer& getGameTimer() const;
 
    private:
-      typedef std::map<Player::UID, Player*> PlayerMap;
-      typedef std::map<net::NetMgr::ConnID, Player*> PlayerConnectionMap;
+      typedef std::map<game::Player::UID, game::Player*> PlayerMap;
+      typedef std::map<net::NetMgr::ConnID, game::Player*> PlayerConnectionMap;
 
       /// The game timer.
-      GameTimer mGameTime;
+      game::GameTimer mGameTime;
 
       /// All players that have actually joined the game.
       PlayerMap mPlayers;
 
       /// All players that have may a connection
       PlayerConnectionMap mConnectedPlayers;
-
-      /// The local player.
-      Player* mLocalPlayer;
    };
 }
 
