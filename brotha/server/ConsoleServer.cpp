a795ad6198d234c3bf8f814431091ff7e31493f8
aegis <aegis@84b32ba4-53c3-423c-be69-77cca6335494>
2002-03-29 20:53:05 +0000

    support command-line arguments
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@906 84b32ba4-53c3-423c-be69-77cca6335494

--- brotha/server/ConsoleServer.cpp
+++ brotha/server/ConsoleServer.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <stdlib.h>
 #include "BrothaServer.h"
 
 
@@ -10,8 +11,41 @@ inline void println(const std::string& s) {
    std::cout << s << std::endl;
 }
 
+bool processcmd(const std::string& cmd) {
+   if (cmd == "startserver" || cmd == "start") {
+      println("Starting game server");
+      g_BrothaServer.StartServer();
+   } else if (cmd == "stopserver" || cmd == "stop") {
+      println("Stopping game server");
+      g_BrothaServer.StopServer();
+   } else if (cmd == "startwebserver" || cmd == "startws") {
+      println("Starting web server");
+      g_BrothaServer.StartWebServer();
+   } else if (cmd == "stopwebserver" || cmd == "stopws") {
+      println("Stopping web server");
+      g_BrothaServer.StopWebServer();
+   } else if (cmd == "exit" || cmd == "quit") {
+      return false;
+   } else if (cmd == "help") {
+      println("help                    - get help");
+      println("startserver, start      - start brothaserva");
+      println("stopserver, stop        - start brothaserva");
+      println("startwebserver, startws - start brothaserva");
+      println("stopwebserver, stopws   - start brothaserva");
+   } else {
+      println("Unknown command: " + cmd);
+   }
+   return true;
+}
+
 
 int main(int argc, char** argv) {
+
+   for (int i = 1; i < argc; ++i) {
+      if (!processcmd(argv[i])) {
+         return EXIT_FAILURE;
+      }
+   }
 
    std::cout << "Console Warn-a-Brotha Server" << std::endl;
    while (true) {
@@ -23,28 +57,8 @@ int main(int argc, char** argv) {
          break;
       }
 
-      if (cmd == "startserver" || cmd == "start") {
-         println("Starting game server");
-         g_BrothaServer.StartServer();
-      } else if (cmd == "stopserver" || cmd == "stop") {
-         println("Stopping game server");
-         g_BrothaServer.StopServer();
-      } else if (cmd == "startwebserver" || cmd == "startws") {
-         println("Starting web server");
-         g_BrothaServer.StartWebServer();
-      } else if (cmd == "stopwebserver" || cmd == "stopws") {
-         println("Stopping web server");
-         g_BrothaServer.StopWebServer();
-      } else if (cmd == "exit" || cmd == "quit") {
+      if (!processcmd(cmd)) {
          break;
-      } else if (cmd == "help") {
-         println("help                    - get help");
-         println("startserver, start      - start brothaserva");
-         println("stopserver, stop        - start brothaserva");
-         println("startwebserver, startws - start brothaserva");
-         println("stopwebserver, stopws   - start brothaserva");
-      } else {
-         println("Unknown command: " + cmd);
       }
    }
 
