245b4bfac91d99e73be0eebaffc01fcc166d996f
lvanoort <lvanoort@84b32ba4-53c3-423c-be69-77cca6335494>
2002-04-04 22:38:04 +0000

    Removed unused code
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@933 84b32ba4-53c3-423c-be69-77cca6335494

--- examples/Shadow/Program6/MainProgram.cpp
+++ examples/Shadow/Program6/MainProgram.cpp
@@ -30,8 +30,6 @@ GLuint grass[1];
 
 LightPosition MyLights[NUMBERLIGHTS];
 
-//GLfloat light_position[] = {0.0, 10.0, 10.0, 1.0};
-//GLfloat light_position2 [] = {0.0, -10.0, 10.0, 1.0};
 GLfloat mat_diffuse[4] = {1.0, 0.5, 0.5, 1.0};
 GLfloat floorPlane[4] = {0.0, 0.0, 1.0, 0.0};
 
