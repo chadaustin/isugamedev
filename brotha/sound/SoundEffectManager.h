71340341c096e573be79597ec8163c31241e5244
nonchocoboy <nonchocoboy@84b32ba4-53c3-423c-be69-77cca6335494>
2002-04-24 21:51:01 +0000

    Added doxy comments and copyright header.
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@1244 84b32ba4-53c3-423c-be69-77cca6335494

--- brotha/sound/SoundEffectManager.h
+++ brotha/sound/SoundEffectManager.h
@@ -1,18 +1,76 @@
+/************************************************************** brotha-head beg
+ *
+ * Warn-a-Brotha
+ *
+ * Original Authors:
+ *    Chad Austin <aegis@aegisknight.org>
+ *    Josh Carlson <josh@chegg.com>
+ *    Jeremy Hanson <jjhanson@iastate.edu>
+ *    Chad Okere <cokere@hatori42.com>
+ *    Ben Scott <bscott@iastate.edu>
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile: SoundEffectManager.h,v $
+ * Date modified: $Date: 2002-04-24 21:51:01 $
+ * Version:       $Revision: 1.2 $
+ * -----------------------------------------------------------------
+ *
+ *********************************************************** brotha-head-end */
+/*************************************************************** brotha-cpr beg
+ *
+ * Warn-a-Brotha
+ * Warn-a-Brotha is (C) Copyright 2002 by
+ *    Chad Austin, Josh Carlson, Jeremy Hanson, Chad Okere, Ben Scott
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ ************************************************************ brotha-cpr-end */
 #ifndef SOUND_SOUND_EFFECT_MANAGER_H
 #define SOUND_SOUND_EFFECT_MANAGER_H
-
 
 #include <string>
 #include <audiere.h>
 
-
 namespace sound {
 
+   /**
+    * Handles the triggering of sound effects for an application. Sounds played
+    * through the SoundEffectManager are generally loaded into memory and played
+    * from there rather than streaming from the disk.
+    */
    class SoundEffectManager {
    public:
+      /**
+       * Creates a new SoundEffectManager for the given Audiere context.
+       *
+       * @param context    the Audiere context to use
+       */
       SoundEffectManager(audiere::Context* context);
+
+      /**
+       * Destroys this manager and terminates all sounds currently being played
+       * by this manager.
+       */
       ~SoundEffectManager();
 
+      /**
+       * Plays the sound contained within the given file.
+       *
+       * @param sound      the filename in which the sound is stored
+       */
       void playSound(const std::string& sound);
 
    private:
@@ -22,8 +80,6 @@ namespace sound {
       audiere::Stream* mStreams[MAX_SOUNDS];
       int mNextStream;
    };
-
 }
-
 
 #endif
