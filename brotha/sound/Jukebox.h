b1475f38ae8b6eb00da5a0142bea3b5970c429d8
nonchocoboy <nonchocoboy@84b32ba4-53c3-423c-be69-77cca6335494>
2002-04-24 21:09:15 +0000

    Added const-correctness where needed. Added some comments. Added copyright header.
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@1242 84b32ba4-53c3-423c-be69-77cca6335494

--- brotha/sound/Jukebox.h
+++ brotha/sound/Jukebox.h
@@ -1,14 +1,67 @@
+/************************************************************** brotha-head beg
+ *
+ * Warn-a-Brotha
+ *
+ * Original Authors:
+ *    Chad Austin <aegis@aegisknight.org>
+ *    Josh Carlson <josh@chegg.com>
+ *    Jeremy Hanson <jjhanson@iastate.edu>
+ *    Chad Okere <cokere@hatori42.com>
+ *    Ben Scott <bscott@iastate.edu>
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile: Jukebox.h,v $
+ * Date modified: $Date: 2002-04-24 21:09:15 $
+ * Version:       $Revision: 1.2 $
+ * -----------------------------------------------------------------
+ *
+ *********************************************************** brotha-head-end */
+/*************************************************************** brotha-cpr beg
+ *
+ * Warn-a-Brotha
+ * Warn-a-Brotha is (C) Copyright 2002 by
+ *    Chad Austin, Josh Carlson, Jeremy Hanson, Chad Okere, Ben Scott
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ ************************************************************ brotha-cpr-end */
 #ifndef SOUND_JUKEBOX_H
 #define SOUND_JUKEBOX_H
-
 
 #include <vector>
 #include <string>
 #include <audiere.h>
 
-
 namespace sound {
 
+   /**
+    * Describes a collection of music tracks that may be queued up so that they
+    * are played in succession. You generally want to get your Jukebox instance
+    * from and instance of the SoundManager class.
+    *
+    * \code
+    *    SoundManager* sndMgr = new SoundManager();
+    *    sndMgr->getJukebox()->addTrack("song1.ogg");
+    *    sndMgr->getJukebox()->addTrack("whatever.wav");
+    *    // ...
+    *    sndMgr->getJukebox()->play();
+    * \endcode
+    *
+    * @see SoundManager
+    */
    class Jukebox {
    public:
       /**
@@ -16,17 +69,21 @@ namespace sound {
        */
       Jukebox(audiere::Context* context);
 
+      /**
+       * Destroys this jukebox. The currently playing track will be stopped and
+       * all memory released.
+       */
       ~Jukebox();
 
       /**
        * Return current size of playlist.
        */
-      int getTrackCount();
+      int getTrackCount() const;
 
       /**
        * Return the filename at position index.
        */
-      std::string getTrack(int index);
+      const std::string& getTrack(int index) const;
 
       /**
        * Append a sound filename to the end of the playlist.
@@ -72,8 +129,6 @@ namespace sound {
       audiere::Stream* mCurrentTrack;
       bool mIsPlaying;
    };
-
 }
-
 
 #endif
