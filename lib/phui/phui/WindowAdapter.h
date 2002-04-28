8b7cc5de58f5e7012818b91abff6670100909dc5
nonchocoboy <nonchocoboy@84b32ba4-53c3-423c-be69-77cca6335494>
2002-04-28 20:24:55 +0000

    Added WindowAdapter helper class.
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@1331 84b32ba4-53c3-423c-be69-77cca6335494

---
+++ lib/phui/phui/WindowAdapter.h
@@ -0,0 +1,88 @@
+/* -*- Mode: C++; tab-width: 3; indent-tabs-mode: nil c-basic-offset: 3 -*- */
+// vim:cindent:ts=3:sw=3:et:tw=80:sta:
+/**************************************************************** phui-head beg
+ *
+ * phui - flexible user interface subsystem
+ *
+ * Original Authors:
+ *    Chad Austin <aegis@aegisknight.org>
+ *    Josh Carlson <josh@chegg.com>
+ *    Ben Scott <bscott@iastate.edu>
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile: WindowAdapter.h,v $
+ * Date modified: $Date: 2002-04-28 20:24:55 $
+ * Version:       $Revision: 1.1 $
+ * -----------------------------------------------------------------
+ *
+ ************************************************************* phui-head-end */
+/***************************************************************** phui-cpr beg
+ *
+ * phui - flexible user interface subsystem
+ * phui is (C) Copyright 2002 by Chad Austin, Josh Carlson, Ben Scott
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ ************************************************************** phui-cpr-end */
+#ifndef PHUI_WINDOW_ADAPTER_H
+#define PHUI_WINDOW_ADAPTER_H
+
+#include "WindowListener.h"
+
+namespace phui {
+
+   /**
+    * Helper class the defines default implementations to all WindowListener
+    * methods. Derive from this class if you want to be notified only of a
+    * particular type of window event instead of all of them.
+    *
+    * @see WindowListener
+    */
+   class WindowAdapter : public WindowListener {
+      WindowAdapter() {}
+      ~WindowAdapter() {}
+
+      /**
+       * Notifies this listener that the source window has opened.
+       *
+       * @param evt  the generated event
+       */
+      void onWindowOpened(const WindowEvent& evt) {}
+
+      /**
+       * Notifies this listener that the source window has closed.
+       *
+       * @param evt  the generated event
+       */
+      void onWindowClosed(const WindowEvent& evt) {}
+
+      /**
+       * Notifies this listener that the source window has gained the focus.
+       *
+       * @param evt  the generated event
+       */
+      void onWindowFocused(const WindowEvent& evt) {}
+
+      /**
+       * Notifies this listener that the source window has lost the focus.
+       *
+       * @param evt  the generated event
+       */
+      void onWindowUnfocused(const WindowEvent& evt) {}
+   };
+}
+
+#endif
