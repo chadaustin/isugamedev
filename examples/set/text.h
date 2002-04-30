c8352a3e6c456cf5e4f31d38ea75c9018491b72a
browner10 <browner10@84b32ba4-53c3-423c-be69-77cca6335494>
2002-04-30 16:40:00 +0000

    allows us to write to the screen using glut
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@1408 84b32ba4-53c3-423c-be69-77cca6335494

---
+++ examples/set/text.h
@@ -0,0 +1,28 @@
+#ifndef TEX
+#define TEX
+
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <stdlib.h>
+#include <string>
+
+#include <GL/glut.h>
+
+//Draws text string in specified screen position
+void glPrintf(const char* output, int x=0, int y=0, void* font=GLUT_BITMAP_HELVETICA_10)
+{
+
+   int size = strlen(output) ;
+   glColor3f(0.0, 0.0, 0.0) ;
+   
+   glRasterPos3f(x, y, 0) ;
+   for (int i=0; i < size; i++)
+   {
+      glutBitmapCharacter( font, output[i]) ;
+   }
+  
+}
+
+
+
+#endif
