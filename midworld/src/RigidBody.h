2ee309aecd4a6a3c6af5b7ac3150e556bae6fda6
nonchocoboy <nonchocoboy@84b32ba4-53c3-423c-be69-77cca6335494>
2002-06-05 01:11:54 +0000

    Started defining the RigidBody interface for the parts needed by the collision detection system.
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@1631 84b32ba4-53c3-423c-be69-77cca6335494

---
+++ midworld/src/RigidBody.h
@@ -0,0 +1,35 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil c-basic-offset: 3 -*- */
+// vim:cindent:ts=3:sw=3:et:tw=80:sta:
+#ifndef MW_RIGID_BODY_H
+#define MW_RIGID_BODY_H
+
+#include <gmtl/AABox.h>
+#include <gmtl/Point.h>
+
+namespace mw
+{
+   /**
+    * Interface for a physical body that cannot be deformed.
+    */
+   class RigidBody
+   {
+   public:
+      virtual ~RigidBody() {}
+
+      /**
+       * Gets the axis-aligned bounding box that tightly contains this body.
+       *
+       * @return  the bounding volume for this body
+       */
+      virtual const gmtl::AABBf& getBounds() const;
+
+      /**
+       * Gets the position of this rigid body relative to the world's origin.
+       *
+       * @return  this body's position.
+       */
+      virtual const gmtl::Point3f& getPosition() const;
+   };
+}
+
+#endif
