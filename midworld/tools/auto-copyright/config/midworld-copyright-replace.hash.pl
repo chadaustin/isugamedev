ed328027e828899406b6b7bab528d75a927db0fc
nonchocoboy <nonchocoboy@84b32ba4-53c3-423c-be69-77cca6335494>
2002-06-06 04:49:37 +0000

    Added the auto-copyright tool.
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@1645 84b32ba4-53c3-423c-be69-77cca6335494

---
+++ midworld/tools/auto-copyright/config/midworld-copyright-replace.hash.pl
@@ -0,0 +1,5 @@
+$begintag = '############################################################## midworld-cpr beg';
+$endtag   = '############################################################## midworld-cpr end';
+
+$newbegintag = "$begintag";
+$newendtag = "$endtag";
