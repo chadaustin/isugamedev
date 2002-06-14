47407586fc236f90f81896d9408acf6e193ad92f
browner10 <browner10@84b32ba4-53c3-423c-be69-77cca6335494>
2002-06-14 14:51:13 +0000

    Here is the *VERY* initial framework of the AI system it is being developed standalone and can be compiled on windows using the supplied project files.  The file test.cpp is where I am running test from.  There is beucoup (that's very much in french) documentation in it, but I don't know docbook commenting style so it may need modifying.  by the end of the weekend I should be able to demonstrate randomely moving objects.  In the meantime, midworld won't be set up to compile in the AI module but if someone wants to add it compile capabilities go right ahead.
    Right now there isn't in most of these files but I wanted to get them up so that it would motivate me to really get after it.  I didn't put them in the namespace mw yet, but I'll add that in when I commit my next round of changes.  Also there are probably ^M at the end of all the files since they came from windows but I haven't checked so I don't know.
    
    
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@1687 84b32ba4-53c3-423c-be69-77cca6335494

---
+++ midworld/src/AI/eventreact.h
@@ -0,0 +1,26 @@
+#ifdef EVENTREACT
+#define EVENTREACT
+
+
+/**
+ * This class is deisgned to be the 
+ */
+
+
+class eventReact
+{
+public:
+   eventReact();
+
+   virtual void event();
+   virtual void reaction();
+   
+private:
+   bool reacting;
+
+};
+
+
+
+
+#endif
