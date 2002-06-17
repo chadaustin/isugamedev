f29ea84260d48bc8410988ed51a6509da4e32a6b
areinot <areinot@84b32ba4-53c3-423c-be69-77cca6335494>
2002-06-17 22:58:57 +0000

    New feature:	Added WeaponPistol.h
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@1698 84b32ba4-53c3-423c-be69-77cca6335494

---
+++ midworld/src/WeaponPistol.h
@@ -0,0 +1,109 @@
+#ifndef MW_WEAPON_PISTOL_H
+#define MW_WEAPON_PISTOL_H
+#include "Weapon.h"
+
+namespace mw
+{
+   /*PISTOL,a basic weapon*/
+   class Pistol : Weapon
+   {
+      private:
+	 int mBusyCounter;
+      	 int mReloadRate;
+	 int mFireRate;
+	 
+      	 int mAmmoInClip;
+	 int mClipSize;
+	 int mAmmoInBag;
+	 int mMaxAmmoInBag;
+	 bool mFiring;
+	 bool mReloading;
+
+      public:
+	 Pistol()
+      	 {
+	    mMaxAmmoInBag=200;
+	    mClipSize=12;
+	    mFiring=false;
+	    mReloading=false;
+	    mBusyCounter=0;
+	    
+	    mReloadRate=1000;
+	    mFireRate=500;	    
+	 }
+	 
+	 void addAmmo(int ammount)  //for ammo pickup
+	 {
+	    mAmmoInBag+=ammount;
+	    if(mAmmoInBag > mMaxAmmoInBag)
+	    {
+	       mAmmoInBag=mMaxAmmoInBag;
+	    }
+	 }
+	 
+      	 void setFiring(bool firing)   //for key pressing edge events
+         {
+      	    mFiring=firing;
+	    if(firing=true)
+	    {
+	       //TODO: This does not belong here!
+	       mBusyCounter=mFireRate;
+	    }	       
+	 }
+	 
+	 void setReloading(bool reloading)   //for key pressing edge events
+         {
+      	    mReloading=reloading;	       
+   	    if(reloading=true)
+   	    {
+      	       //TODO: This does not belong here!
+	       mBusyCounter=mReloadRate;
+	    }	       
+	 }
+	 
+      	 bool isFiring()
+	       {return mFiring;}
+	 
+	 bool isReloading()
+	       {return mReloading;}
+	 
+	 
+	 RigidBody* createBullet()
+      	 //pre:  Weapon can be fired, and there
+	 //      is a bullet in the chamber
+	 {	    
+	    RigidBody *bullet=new RigidBody;
+
+	    //TODO: set bullet properties here
+	    
+	    mAmmoInClip--;
+	    return bullet;
+	 }
+	 
+	 bool canFire()const
+	 {
+	    if(!mReloading && !mFiring && mAmmoInClip>0)
+	    {  return true;   }	    
+	    //else
+	       return false;
+	 }
+	 
+	 void update(u64 dt)
+	 {
+	    dt/=1000;
+	    mBusyCounter-=dt;
+	    if(mBusyCounter<=0)
+	    {
+	       //make sure busyCounter is 0
+	       mBusyCounter=0;
+	       
+	       //reset flags
+	       mReloading=false;
+	       mfiring=false;
+	    }
+	 }	    
+   };
+}
+
+
+#endif
