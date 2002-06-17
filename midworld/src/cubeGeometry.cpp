def385a9ad5299be3ad11f12d619351c3780eefd
nonchocoboy <nonchocoboy@84b32ba4-53c3-423c-be69-77cca6335494>
2002-06-17 04:36:15 +0000

    GL include needs to be in the header.
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@1693 84b32ba4-53c3-423c-be69-77cca6335494

--- midworld/src/cubeGeometry.cpp
+++ midworld/src/cubeGeometry.cpp
@@ -1,8 +1,3 @@
-#ifdef WIN32
-    #include <windows.h>
-#endif
-#include <GL/gl.h>    // OpenGL
-
 #include "cubeGeometry.h"
 
 // number of verticies
--- midworld/src/cubeGeometry.h
+++ midworld/src/cubeGeometry.h
@@ -1,13 +1,18 @@
 #ifndef CUBE_GEOM___
 #define CUBE_GEOM___
 
+#ifdef WIN32
+#  include <windows.h>
+#endif
+#include <GL/gl.h>
+
 class cubeGeometry
 {
 public:
    // number of verticies
    // this can be passed to opengl's glDrawArrays( .., .. );
    unsigned int size() const { return cubeGeometry::mSize; }
-   
+
    // format is: T2F_C4F_N3F_V3F
    float* data() { return (float*)cubeGeometry::mData; }
    const float* data() const { return (float*)cubeGeometry::mData; }
@@ -18,7 +23,7 @@ public:
       glInterleavedArrays( GL_T2F_C4F_N3F_V3F, 0, (float*)mData ); //GL_T2F_C4F_N3F_V3F format
       glDrawArrays( GL_TRIANGLES, 0, mSize );
    }
-   
+
 private:
    static const unsigned int mSize;
    static const float mData[432];
