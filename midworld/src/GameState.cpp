65d648abcee7548fba2117105859aee06a01a213
nonchocoboy <nonchocoboy@84b32ba4-53c3-423c-be69-77cca6335494>
2002-06-17 04:31:01 +0000

    Split GameState into header and implementation.
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@1692 84b32ba4-53c3-423c-be69-77cca6335494

---
+++ midworld/src/GameState.cpp
@@ -0,0 +1,200 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil c-basic-offset: 3 -*- */
+// vim:cindent:ts=3:sw=3:et:tw=80:sta:
+#include "GameState.h"
+#include "IntroState.h"
+#include "MenuState.h"
+
+namespace mw
+{
+   GameState::GameState()
+      : mSpeed(10)
+      , mAccelerate(UP)
+      , mReverse(UP)
+      , mStrafeRight(UP)
+      , mStrafeLeft(UP)
+   {}
+
+   GameState::~GameState()
+   {}
+
+   void
+   GameState::update(u64 elapsedTime)
+   {
+      double dt = ((double)elapsedTime) / 1000000.0;
+      mCamera.setPlayerPos(mPlayer.position());
+
+      // Accelerate
+      if (mAccelerate == EDGE_DOWN)
+      {
+         mPlayer.setVelocity(mPlayer.velocity() + gmtl::Vec3f(0,0,-mSpeed));
+      }
+      else if (mAccelerate == EDGE_UP)
+      {
+         mPlayer.setVelocity(mPlayer.velocity() - gmtl::Vec3f(0,0,-mSpeed));
+      }
+
+      // Reverse
+      if (mReverse == EDGE_DOWN)
+      {
+         mPlayer.setVelocity(mPlayer.velocity() + gmtl::Vec3f(0,0,mSpeed*0.7f));
+      }
+      else if (mReverse == EDGE_UP)
+      {
+         mPlayer.setVelocity(mPlayer.velocity() - gmtl::Vec3f(0,0,mSpeed*0.7f));
+      }
+
+      // Strafe left
+      if (mStrafeLeft == EDGE_DOWN)
+      {
+         mPlayer.setVelocity(mPlayer.velocity() + gmtl::Vec3f(-mSpeed*0.9f,0,0));
+      }
+      else if (mStrafeLeft == EDGE_UP)
+      {
+         mPlayer.setVelocity(mPlayer.velocity() - gmtl::Vec3f(-mSpeed*0.9f,0,0));
+      }
+
+      // Strafe right
+      if (mStrafeRight == EDGE_DOWN)
+      {
+         mPlayer.setVelocity(mPlayer.velocity() + gmtl::Vec3f(mSpeed*0.9f,0,0));
+      }
+      else if (mStrafeRight == EDGE_UP)
+      {
+         mPlayer.setVelocity(mPlayer.velocity() - gmtl::Vec3f(mSpeed*0.9f,0,0));
+      }
+
+      // update edge states...
+      updateEdgeState(mAccelerate);
+      updateEdgeState(mReverse);
+      updateEdgeState(mStrafeRight);
+      updateEdgeState(mStrafeLeft);
+
+      // Iterate over all the rigid bodies and update them
+      for (RigidBodyList::iterator itr = mBodies.begin(); itr != mBodies.end(); ++itr)
+      {
+         (*itr)->update(elapsedTime);
+      }
+
+      mCamera.update(dt);
+      mPlayer.update(dt);
+   }
+
+   void
+   GameState::draw()
+   {
+      glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+      glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+      glEnable(GL_DEPTH_TEST);
+      glEnable(GL_BLEND);
+      glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+      glMatrixMode(GL_PROJECTION);
+      glLoadIdentity();
+
+      /// @todo get rid of this...     maybe the clearing buffer too...
+      gluPerspective(80.0f, 4.0f/3.0f, 0.01f, 10000.0f);
+
+      // initialize your matrix stack used for transforming your models
+      glMatrixMode(GL_MODELVIEW);
+      glLoadIdentity();
+
+      glPushMatrix();
+         mCamera.draw();
+         mPlayer.draw();
+         mScene.draw();
+
+         // Draw all the bodies in the world
+         for (RigidBodyList::iterator itr = mBodies.begin(); itr != mBodies.end(); ++itr)
+         {
+            (*itr)->draw();
+         }
+      glPopMatrix();
+   }
+
+   void
+   GameState::onKeyPress(SDLKey sym, bool down)
+   {
+      // todo replace this with a keymapper.
+      // map keys to events... yay.
+      switch (sym)
+      {
+      case SDLK_w: case SDLK_UP:
+         updateEdgeState(mAccelerate, down);
+         break;
+      case SDLK_s: case SDLK_DOWN:
+         updateEdgeState(mReverse, down);
+         break;
+      case SDLK_a: case SDLK_LEFT:
+         updateEdgeState(mStrafeLeft, down);
+         break;
+      case SDLK_d: case SDLK_RIGHT:
+         updateEdgeState(mStrafeRight, down);
+         break;
+      case SDLK_ESCAPE: case SDLK_q:
+         if (down)
+         {
+            invokeTransition(new MenuState());
+         }
+         break;
+      }
+   }
+
+   void
+   GameState::onMousePress(Uint8 button, bool down, int x, int y)
+   {
+      if (button == SDL_BUTTON_LEFT)
+      {
+         updateEdgeState(mShoot, down);
+      }
+   }
+
+   void
+   GameState::onMouseMove(int x, int y)
+   {
+      float screen_size_x = 640;
+      float screen_size_y = 480;
+      gmtl::Vec3f mid(screen_size_x / 2, screen_size_y / 2, 0);
+      gmtl::Vec3f pos(x, y, 0);
+      gmtl::Vec3f dir(pos - mid);
+      gmtl::normalize(dir);
+
+      float rad = gmtl::Math::aCos(gmtl::dot(dir, gmtl::Vec3f(1,0,0)));
+      gmtl::Vec3f side = gmtl::cross(dir, gmtl::Vec3f(1,0,0));
+
+      // move so it points in mouse dir...
+      rad += gmtl::Math::deg2Rad(90.0f);
+
+      // map the dot (angle) and cross (side) to the player
+      if (side[2] >= 0.0f)
+      {
+         mPlayer.setRot(rad + gmtl::Math::deg2Rad(180.0f));
+      }
+      else
+      {
+         mPlayer.setRot(-rad);
+      }
+   }
+
+   void
+   GameState::updateEdgeState(EdgeState& state, bool absoluteState)
+   {
+      switch (state)
+      {
+         case DOWN: if (!absoluteState) state = EDGE_UP; break;
+         case UP: if (absoluteState) state = EDGE_DOWN; break;
+         case EDGE_DOWN: if (absoluteState) state = DOWN; break;
+         case EDGE_UP: if (!absoluteState) state = UP; break;
+      }
+   }
+
+   void
+   GameState::updateEdgeState(EdgeState& state)
+   {
+      switch (state)
+      {
+         case EDGE_DOWN: state = DOWN; break;
+         case EDGE_UP: state = UP; break;
+         default: break;
+      }
+   }
+}
--- midworld/src/GameState.h
+++ midworld/src/GameState.h
@@ -4,189 +4,61 @@
 #ifndef MW_GAMESTATE_H
 #define MW_GAMESTATE_H
 
+#include <vector>
+
 #include <SDL.h>
 #include "State.h"
 #include "GameScene.h"
 #include "Camera.h"
 #include "Player.h"
-#include "IntroState.h"
+#include "RigidBody.h"
 
 namespace mw
 {
-
    class GameState : public State
    {
    public:
-      GameState()
-         : mSpeed(10)
-         , mAccelerate(UP)
-         , mReverse(UP)
-         , mStrafeRight(UP)
-         , mStrafeLeft(UP)
-      {
-      }
-      
-      virtual ~GameState()
-      {
-      }
+      GameState();
+      ~GameState();
 
       enum EdgeState
       {
          DOWN, UP, EDGE_DOWN, EDGE_UP
       };
-         
-      virtual void update( u64 elapsedTime )
-      {
-         double time_delta = ((double)elapsedTime) / 1000000.0;
-         mCamera.setPlayerPos( mPlayer.position() );
-         
-         // process input events:
-         if (mAccelerate == EDGE_DOWN)
-            mPlayer.setVelocity( mPlayer.velocity() + gmtl::Vec3f( 0,0, -mSpeed ) );
-         else if (mAccelerate == EDGE_UP)
-            mPlayer.setVelocity( mPlayer.velocity() - gmtl::Vec3f( 0,0, -mSpeed ) );
-         
-         if (mReverse == EDGE_DOWN)
-            mPlayer.setVelocity( mPlayer.velocity() + gmtl::Vec3f( 0,0, mSpeed*0.7f ) );
-         else if (mReverse == EDGE_UP)
-            mPlayer.setVelocity( mPlayer.velocity() - gmtl::Vec3f( 0,0, mSpeed*0.7f ) );
-         
-         if (mStrafeLeft == EDGE_DOWN)
-            mPlayer.setVelocity( mPlayer.velocity() + gmtl::Vec3f( -mSpeed*0.9f,0,0 ) );
-         else if (mStrafeLeft == EDGE_UP)
-            mPlayer.setVelocity( mPlayer.velocity() - gmtl::Vec3f( -mSpeed*0.9f,0,0 ) );
-         
-         if (mStrafeRight == EDGE_DOWN)
-            mPlayer.setVelocity( mPlayer.velocity() + gmtl::Vec3f( mSpeed*0.9f,0,0 ) );
-         else if (mStrafeRight == EDGE_UP)
-            mPlayer.setVelocity( mPlayer.velocity() - gmtl::Vec3f( mSpeed*0.9f,0,0 ) );
-         
-         // update edge states...
-         this->updateEdgeState( mAccelerate );
-         this->updateEdgeState( mReverse );
-         this->updateEdgeState( mStrafeRight );
-         this->updateEdgeState( mStrafeLeft );
-         
-         
-         mCamera.update( time_delta );
-         mPlayer.update( time_delta );
-      }
 
-      virtual void draw()
-      {
-         glClearColor( 0.0f, 0.0f, 0.0f, 1.0f );
-         glClear( GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT );
-         glEnable( GL_DEPTH_TEST );
-         glEnable( GL_BLEND );
-         glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA ); 
+      void update(u64 elapsedTime);
 
-         glMatrixMode( GL_PROJECTION );
-         glLoadIdentity();         
+      void draw();
 
-         /// @todo get rid of this...     maybe the clearing buffer too...       
-         gluPerspective( 80.0f, 4.0f/3.0f, 0.01f, 10000.0f );
+      void onKeyPress(SDLKey sym, bool down);
 
-         // initialize your matrix stack used for transforming your models
-         glMatrixMode( GL_MODELVIEW );
-         glLoadIdentity();      
-   
-         glPushMatrix();
-            mCamera.draw();
-            mPlayer.draw();
-            mScene.draw();
-         glPopMatrix();
-      }
+      void onMousePress(Uint8 button, bool down, int x, int y);
 
-      virtual void onKeyPress( SDLKey sym, bool down )
-      {
-         // todo replace this with a keymapper.
-         // map keys to events... yay.
-         switch (sym)
-         {
-           case SDLK_w: case SDLK_UP:
-             this->updateEdgeState( mAccelerate, down );
-             break;
-           case SDLK_s: case SDLK_DOWN:
-             this->updateEdgeState( mReverse, down );
-             break;
-           case SDLK_a: case SDLK_LEFT:
-             this->updateEdgeState( mStrafeLeft, down );
-             break;
-           case SDLK_d: case SDLK_RIGHT:
-             this->updateEdgeState( mStrafeRight, down );
-             break;
-           case SDLK_ESCAPE: case SDLK_q:
-             if (down) this->invokeTransition( new MenuState ); 
-             break;
-         }
-      }
-            
-      virtual void onMousePress( Uint8 button, bool down, int x, int y )
-      {
-         
-         
-      }
-      
-      virtual void onMouseMove( int x, int y )
-      {
-         float screen_size_x = 640;
-         float screen_size_y = 480;
-         gmtl::Vec3f mid( screen_size_x / 2, screen_size_y / 2, 0 );
-         gmtl::Vec3f pos( x, y, 0 );
-         gmtl::Vec3f dir( pos - mid );
-         gmtl::normalize( dir );
-         
-         float rad = gmtl::Math::aCos( gmtl::dot( dir, gmtl::Vec3f( 1,0,0 ) ) );
-         gmtl::Vec3f side = gmtl::cross( dir, gmtl::Vec3f( 1,0,0 ) );
-         
-         // move so it points in mouse dir...
-         rad += gmtl::Math::deg2Rad( 90.0f );
-         
-         // map the dot (angle) and cross (side) to the player
-         if (side[2] >= 0.0f)
-         {
-            mPlayer.setRot( rad + gmtl::Math::deg2Rad( 180.0f ) );
-         }
-         else
-         {
-            mPlayer.setRot( -rad );
-         }
-      }
+      void onMouseMove(int x, int y);
 
    private:
       // called on input change
-      void updateEdgeState( EdgeState& state, bool absoluteState )
-      {
-         switch (state)
-         {
-            case DOWN: if (!absoluteState) state = EDGE_UP; break;
-            case UP: if (absoluteState) state = EDGE_DOWN; break;
-            case EDGE_DOWN: if (absoluteState) state = DOWN; break;
-            case EDGE_UP: if (!absoluteState) state = UP; break;
-         }         
-      }
+      void updateEdgeState(EdgeState& state, bool absoluteState);
+
       // called on update for each button
-      void updateEdgeState( EdgeState& state )
-      {
-         switch (state)
-         {
-            case EDGE_DOWN: state = DOWN; break;
-            case EDGE_UP: state = UP; break;
-            default: break;
-         }         
-      }
-         
+      void updateEdgeState(EdgeState& state);
+
+   private:
       State* mNextState;
       bool mIsQuitting;
       GameScene mScene;
       Camera mCamera;
       float mSpeed;
       Player mPlayer;
-      
+
+      typedef std::vector<RigidBody*> RigidBodyList;
+      RigidBodyList mBodies;
+
       // actions :*)
       EdgeState mAccelerate;
       EdgeState mReverse;
       EdgeState mStrafeRight, mStrafeLeft;
+      EdgeState mShoot;
    };
 
 }
