ab567ba01f5c4f65c526907d6df4ad7ed7563a8e
browner10 <browner10@84b32ba4-53c3-423c-be69-77cca6335494>
2002-06-20 21:12:23 +0000

    Abstract Base class for an observer
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@1715 84b32ba4-53c3-423c-be69-77cca6335494

---
+++ midworld/src/AI/Observer.h
@@ -0,0 +1,33 @@
+#ifndef OBS
+#define OBS
+
+#include <vector>
+
+
+class Subject;
+
+class Observer
+{
+public:
+   virtual ~Observer(){};
+   virtual void Update(Subject* TheChangedSubject) = 0;
+protected:
+   Observer(){};
+};
+
+
+class Subject 
+{
+public:
+   virtual ~Subject(){};
+
+   virtual void Attach(Observer*);
+   virtual void Detach(Observer*);
+   virtual void Notify();
+protected:
+   Subject(){};
+private:
+   std::vector<Observer*> _Observers;
+};
+
+#endif
