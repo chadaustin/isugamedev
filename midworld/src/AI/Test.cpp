daba76679b8ea6edcefa809e8e2f210d6ee6f189
browner10 <browner10@84b32ba4-53c3-423c-be69-77cca6335494>
2002-06-20 21:15:42 +0000

    An application to test the system
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@1718 84b32ba4-53c3-423c-be69-77cca6335494

---
+++ midworld/src/AI/Test.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <AISystem.h>
+#include <vector>
+#include <Instinct.h>
+#include <Behavior.h>
+#include <Unit.h>
+#include <Shoot.h>
+#include <BoolBehavior.h>
+
+int main(int argc, char* argv[]){
+   AISystem AI;
+   std::string Unit1 = "Ben";
+   std::string Unit2 = "Chad";
+   std::string Unit3 = "Kevin";
+   std::string Unit4 = "Jon";
+   std::string Unit5 = "Andres";
+   AINode Node1(Unit1, -1, 1);
+   AINode Node2(Unit2, -1, 1);
+   AINode Node3(Unit3, -1, 1);
+   AINode Node4(Unit4, -1, 1);
+   AINode Node5(Unit5, -1, 1);
+  
+   Behavior* First;
+   Behavior* Second;
+   
+   Behavior* Third;
+   Behavior* Fourth;
+
+   
+
+   BehaviorFactory B;
+   Shoot S;
+   
+   First = CreateBehavior(S);
+   Second = CreateBehavior(B);
+   
+   BoolBehavior Test;
+   BoolBehavior Test2;
+ //  Test2.SetFalse();
+  
+   Instinct* Shooting = new Instinct(Node2.InstinctMan, *First, *Third);
+   Instinct* Shooting2 = new Instinct(Node1.InstinctMan, *Second, *Fourth);
+   
+   AI.RegisterNode(Node1);
+   AI.RegisterNode(Node2);
+   AI.RegisterNode(Node3);
+   AI.RegisterNode(Node4);
+   AI.RegisterNode(Node5);
+
+
+
+   
+
+   AI.Update();
+   AI.Update();
+   AI.Update();
+   
+
+
+   
+ 	return 0;
+}
