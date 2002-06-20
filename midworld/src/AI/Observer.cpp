45160f66d2736f8772e0e6c93c69901dede37d7c
browner10 <browner10@84b32ba4-53c3-423c-be69-77cca6335494>
2002-06-20 21:11:57 +0000

    abstract bass class implementation of an observer
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@1714 84b32ba4-53c3-423c-be69-77cca6335494

---
+++ midworld/src/AI/Observer.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <Observer.h>
+
+void Subject::Attach(Observer* o) {
+   _Observers.push_back(o);
+}
+
+void Subject::Detach (Observer* o) {
+   std::cout << "Detach not implemented - Your stuck with it" << std::endl;
+}
+
+void Subject::Notify(){
+   std::vector<Observer*>::iterator itr;
+   for(itr = _Observers.begin();itr!=_Observers.end();itr++){
+      (*itr)->Update(this);
+   }
+}
