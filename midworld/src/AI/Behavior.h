90629d58ef4480ce2ccc33c1d0af7ded5a191a98
browner10 <browner10@84b32ba4-53c3-423c-be69-77cca6335494>
2002-06-20 21:05:45 +0000

    Behavior holds the factory method for creating behaviors and a behavior holds a serious of ActionContainers to be opperated. this file replaces behavior.h in the repos
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@1709 84b32ba4-53c3-423c-be69-77cca6335494

---
+++ midworld/src/AI/Behavior.h
@@ -0,0 +1,58 @@
+#ifndef BEHAVIOR
+#define BEHAVIOR
+
+#include <iostream>
+#include <ActionContainer.h>
+
+
+
+
+
+
+class Behavior 
+{
+public:
+   Behavior(){};
+   
+   virtual void AddActionContainer(ActionContainer* AC)
+   {
+      Actions = AC;
+   }
+   ActionContainer* GetAction(){
+      return Actions;
+   }
+
+private:
+   ActionContainer* Actions;
+};
+
+
+class BehaviorFactory
+{
+public:
+   BehaviorFactory(){};
+
+   virtual Behavior* MakeBehavior()
+   {
+      return new Behavior;
+   }
+   virtual ActionContainer* MakeActionContainer() const
+   {
+      return new ActionContainer;
+   }
+};
+
+
+
+Behavior* CreateBehavior(BehaviorFactory &Factory){
+   Behavior* TheBehavior = Factory.MakeBehavior();
+   ActionContainer* TheAction = Factory.MakeActionContainer();
+   TheBehavior->AddActionContainer(TheAction);
+   return TheBehavior;
+}
+
+
+
+
+
+#endif
