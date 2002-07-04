4fafda96cbf5c760a6ce34c4c3eddec9021f7f28
nonchocoboy <nonchocoboy@84b32ba4-53c3-423c-be69-77cca6335494>
2002-07-04 21:19:51 +0000

    Made this closer to the coding standard.
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@1783 84b32ba4-53c3-423c-be69-77cca6335494

--- midworld/src/Shotgun.h
+++ midworld/src/Shotgun.h
@@ -13,14 +13,11 @@ namespace mw
    /*SHOTGUN,a basic weapon*/
    class  Shotgun: public BaseProjectileWeapon
    {
-   private:
-	       float mSpreadAngle;
    public:
-
       Shotgun() : BaseProjectileWeapon()
       {
-	 mSpreadAngle=10.f;
-	 srand(SDL_GetTicks());
+         mSpreadAngle=10.f;
+         srand(SDL_GetTicks());
          mMaxAmmoInBag = 1200;
          mClipSize = 6;
          mFiring = false;
@@ -37,16 +34,16 @@ namespace mw
          mBusyCounter = 0.0f;
          mReloadCounter = 0.0f;
       }
-      
+
       /** return the Player slot number that the weapon goes in. */
       virtual int getType() { return 3; }
-      
+
       /** render the weapon using opengl calls. */
-      virtual void draw() const 
+      virtual void draw() const
       {
          glPushMatrix();
-            glMultMatrixf( this->matrix().getData() );
-            glScalef( 0.15f, 0.15f, 0.8f );
+            glMultMatrixf(this->matrix().getData());
+            glScalef(0.15f, 0.15f, 0.8f);
             cubeGeometry().render();
          glPopMatrix();
       }
@@ -55,50 +52,39 @@ namespace mw
    protected:
       RigidBody* Shotgun::createBullet()
       {
-	 
          RigidBody* bullet = new RigidBody();
          bullet->setVel(gmtl::Vec3f(0,0,-30));
-
-
-	 
          return bullet;
       }
-	 
-      virtual void emitBullet( GameState& g )
+
+      virtual void emitBullet(GameState& g)
       {
-	 
-                     
          // add the bullet to the gamestate...
-         
-	 RigidBody** bullet;
-	 bullet=new RigidBody *[NUM_PELLETS];
-	 
-	
-	 
-	 
-	 for(unsigned int i=0;i<NUM_PELLETS;i++)
-	 {
-	     //TODO:  seed this according to world time?
-	    
+         RigidBody** bullet;
+         bullet = new RigidBody*[NUM_PELLETS];
+
+         for(unsigned int i=0; i<NUM_PELLETS; i++)
+         {
+            //TODO:  seed this according to world time?
+
 
             //generate random bullet directions with the sweep of mSpreadAngle
-	    //degrees	    
-	    
-      	    float angle = gmtl::Math::deg2Rad((mSpreadAngle* float(rand())/RAND_MAX) - (mSpreadAngle/2));
-	    
-            gmtl::Quatf r( gmtl::make<gmtl::Quatf>( gmtl::AxisAnglef( angle, 0.0f, 1.0f, 0.0f ) ) );
+            //degrees
+            float angle = gmtl::Math::deg2Rad((mSpreadAngle* float(rand())/RAND_MAX) - (mSpreadAngle/2));
 
-	    bullet[i] = this->createBullet();
-	         
-	    bullet[i]->setRot( this->getRot() * r ); //shoot off at a limited random angle
-            bullet[i]->setPos( this->getPos() );
-            bullet[i]->setVel( this->getRot() * r * bullet[i]->getVel() );
-	    
-            g.add( bullet[i] ); // bullet is not mine anymore, belongs to GameState
-	    
-	 }
-	 
-	 // Remove the spent ammo from the clip
+            gmtl::Quatf r(gmtl::make<gmtl::Quatf>(gmtl::AxisAnglef(angle, 0.0f, 1.0f, 0.0f)));
+
+            bullet[i] = this->createBullet();
+
+            bullet[i]->setRot(this->getRot() * r); //shoot off at a limited random angle
+            bullet[i]->setPos(this->getPos());
+            bullet[i]->setVel(this->getRot() * r * bullet[i]->getVel());
+
+            g.add(bullet[i]); // bullet is not mine anymore, belongs to GameState
+
+         }
+
+         // Remove the spent ammo from the clip
          --mAmmoInClip;
 
          // Start the reload process automagically if necessary
@@ -111,9 +97,10 @@ namespace mw
          {
             mBusyCounter = mFireRate;
          }
-
-
       }
+
+   private:
+      float mSpreadAngle;
    };
 }
 
