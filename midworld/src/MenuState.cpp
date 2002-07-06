398c05323ed41bbfc0a9c7aca96383e24c897eec
nonchocoboy <nonchocoboy@84b32ba4-53c3-423c-be69-77cca6335494>
2002-07-06 05:02:27 +0000

    Coding style.
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@1798 84b32ba4-53c3-423c-be69-77cca6335494

--- midworld/src/MenuState.cpp
+++ midworld/src/MenuState.cpp
@@ -7,65 +7,57 @@
 
 #include <math.h>
 
-
 namespace mw
 {
-
-
    MenuState::MenuState()
    {
       //setup images
       mImages.resize(3);
-    
+
       mImages[0] = new Texture("menu_bg.jpeg");
-      mImages[1] = new Texture("menu_selection.png");      
-      mImages[2] = new Texture("menu_square.png");      
-      
-      //setup menu items      
+      mImages[1] = new Texture("menu_selection.png");
+      mImages[2] = new Texture("menu_square.png");
+
+      //setup menu items
       mItemList.resize(5);
-      
-      for(int i=0;i<5;i++)
+
+      for(int i=0; i<5; i++)
       {
-      	 mItemList[i].x=70;
-         mItemList[i].y=260+28*i;
-      	 mItemList[i].height=25;
+         mItemList[i].x = 70;
+         mItemList[i].y = 260 + 28*i;
+         mItemList[i].height = 25;
       }
 
-      mItemList[MENU_NEW].width=115;
-      mItemList[MENU_LOAD].width=120;
-      mItemList[MENU_OPTIONS].width=90;
-      mItemList[MENU_CREDITS].width=83;
-      mItemList[MENU_QUIT].width=55;
-      
+      mItemList[MENU_NEW].width = 115;
+      mItemList[MENU_LOAD].width = 120;
+      mItemList[MENU_OPTIONS].width = 90;
+      mItemList[MENU_CREDITS].width = 83;
+      mItemList[MENU_QUIT].width = 55;
+
       mCurrentItem = 0;
-      
+
       //setup decorative squares and paths
       mSquares.resize(4);
 
-      for(int i=0;i<4;i++)      
+      for(int i=0; i<4; i++)
       {
-	  mSquares[i].path.resize(4);
+         mSquares[i].path.resize(4);
       }
 
-      
       //this is the path we want all the squares to take
+      mSquares[0].path[0] = MenuPathTarget(600, 140, 0.1f);
+      mSquares[0].path[1] = MenuPathTarget(600, 240, 0.1f);
+      mSquares[0].path[2] = MenuPathTarget(300, 240, 0.1f);
+      mSquares[0].path[3] = MenuPathTarget(300, 140, 0.1f);
 
-      mSquares[0].path[0]= MenuPathTarget(600, 140, 0.1f);
-      mSquares[0].path[1]= MenuPathTarget(600, 240, 0.1f);
-      mSquares[0].path[2]= MenuPathTarget(300, 240, 0.1f);
-      mSquares[0].path[3]= MenuPathTarget(300, 140, 0.1f);
-      
-      for(unsigned int i=0;i<4;i++)
+      for(unsigned int i=0; i<4; i++)
       {
-	 
-      	 mSquares[i].path = mSquares[0].path;
-	 
-	 
-	 mSquares[i].targetIndex=1; //head towards first path target
-	 mSquares[i].x=100 + 15*i;
-	 mSquares[i].y=240;
-	 mSquares[i].width=mSquares[i].height=10;
-	 mSquares[i].alpha=0.3f;
+         mSquares[i].path = mSquares[0].path;
+         mSquares[i].targetIndex = 1; //head towards first path target
+         mSquares[i].x = 100 + 15*i;
+         mSquares[i].y = 240;
+         mSquares[i].width = mSquares[i].height = 10;
+         mSquares[i].alpha = 0.3f;
       }
    }
 
@@ -78,9 +70,7 @@ namespace mw
       mImages.clear();
    }
 
-
-   
-   void 
+   void
    MenuState::update(u64 elapsedTime)
    {
       //TODO: make this work!  Andres
@@ -91,21 +81,21 @@ namespace mw
       {
 
 	 //all temporary variables, because the struct path is long
-	 
+	
 	 float x,y,tx,ty,dx,dy;  //distance to target
 	 float newX,newY;  //next position of square
 	 float speed;
-      	 
-	 
+      	
+	
 	 x=mSquares[i].x;
 	 y=mSquares[i].y;
-	 
+	
 	 tx=mSquares[i].path[mSquares[i].targetIndex].x;
 	 ty=mSquares[i].path[mSquares[i].targetIndex].y;
-	 
+	
 	 speed = mSquares[i].path[mSquares[i].targetIndex].approachSpeed;
-	 
-	 
+	
+	
 	 //distance components to target
       	 dx=tx-x;
       	 dy=ty-y;
@@ -136,7 +126,7 @@ namespace mw
       }*/
    }
 
-   void 
+   void
    MenuState::draw()
    {
       glEnable(GL_TEXTURE_2D);
@@ -157,18 +147,18 @@ namespace mw
       glMatrixMode(GL_MODELVIEW);
       glLoadIdentity();
 
+      // draw background
       glColor4f(1, 1, 1, 1);
-      mImages[0]->drawRectangle(0,0,640,480);	// draw background
-      
-     
-      
+      mImages[0]->drawRectangle(0,0,640,480);
+
       // draw selection blur
       glColor4f(.8f,.9f,1,0.5f);
-      mImages[1]->drawRectangle( mItemList[mCurrentItem].x,
-	             	      	 mItemList[mCurrentItem].y,
-	             	      	 mItemList[mCurrentItem].x + mItemList[mCurrentItem].width,
-	             	      	 mItemList[mCurrentItem].y + mItemList[mCurrentItem].height);
-/*    //draw each square  
+      mImages[1]->drawRectangle(
+            mItemList[mCurrentItem].x,
+            mItemList[mCurrentItem].y,
+            mItemList[mCurrentItem].x + mItemList[mCurrentItem].width,
+            mItemList[mCurrentItem].y + mItemList[mCurrentItem].height);
+/*    //draw each square
       for(int i=0;i<int(mSquares.size());i++)
       {
 	 glColor4f(1,1,1,mSquares[i].alpha);
@@ -178,8 +168,7 @@ namespace mw
       	             	      	    mSquares[i].y + mSquares[i].height);
 	
       }
-  */    
-     
+  */
    }
 
    void
@@ -195,9 +184,9 @@ namespace mw
             case SDLK_UP:
                mCurrentItem = (mCurrentItem + 4) % 5;
                break;
-	    case 27:
-	       quit();
-	       break;
+            case 27:
+               quit();
+               break;
             case SDLK_RETURN:
                switch (mCurrentItem)
                {
@@ -227,5 +216,4 @@ namespace mw
       mMouseX = x;
       mMouseY = y;
    }
-
 }
