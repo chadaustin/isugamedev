2d922d03bea3f5d352055b0a6e5b84380cae46d4
nonchocoboy <nonchocoboy@84b32ba4-53c3-423c-be69-77cca6335494>
2002-09-17 02:51:57 +0000

    Remove the tmp directory if it is empty.
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@1916 84b32ba4-53c3-423c-be69-77cca6335494

--- midworld/get-resources.sh
+++ midworld/get-resources.sh
@@ -4,3 +4,9 @@ wget --directory-prefix tmp http://isugamedev.sourceforge.net/midworld/data/reso
 mkdir -p build
 (cd build && unzip ../tmp/resources.zip)
 rm -f tmp/resources.zip
+
+# Remove the directory if it's not empty
+FILES=`ls tmp`
+if test "x$FILES" == "x"; then
+   rmdir tmp
+fi
