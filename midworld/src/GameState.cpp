1c5bce69cfcb4f58b9118ea0f3f59aa0b6861b77
nonchocoboy <nonchocoboy@84b32ba4-53c3-423c-be69-77cca6335494>
2002-09-20 08:21:48 +0000

    Use Andres' security droid model.
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@1931 84b32ba4-53c3-423c-be69-77cca6335494

--- midworld/src/GameState.cpp
+++ midworld/src/GameState.cpp
@@ -24,8 +24,8 @@
  *
  * -----------------------------------------------------------------
  * File:          $RCSfile: GameState.cpp,v $
- * Date modified: $Date: 2002-09-18 00:30:39 $
- * Version:       $Revision: 1.31 $
+ * Date modified: $Date: 2002-09-20 08:21:48 $
+ * Version:       $Revision: 1.32 $
  * -----------------------------------------------------------------
  *
  ********************************************************** midworld-cpr-end */
@@ -77,7 +77,7 @@ namespace mw
 
       // Init some of the model resources
       ResourceManager* res_mgr = GameManager::instance().getResourceManager();
-      res_mgr->add("security_droid",   "models/security_droid.wrl");
+      res_mgr->add("security_droid",   "models/security_droid.obj");
       res_mgr->add("bullet",           "models/bullet.obj");
 
       // Init the collision detection system
