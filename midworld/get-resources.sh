911688764514c9abffa2238ba98900150455c0cc
aegis <aegis@84b32ba4-53c3-423c-be69-77cca6335494>
2002-09-20 10:24:18 +0000

    remove resources.zip if it exists so wget doesn't download resources.zip.1
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@1935 84b32ba4-53c3-423c-be69-77cca6335494

--- midworld/get-resources.sh
+++ midworld/get-resources.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+rm -f tmp/resources.zip
 wget --directory-prefix tmp http://isugamedev.sourceforge.net/midworld/data/resources.zip
 mkdir -p build
 (cd build && unzip ../tmp/resources.zip)
