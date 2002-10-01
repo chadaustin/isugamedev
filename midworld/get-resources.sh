307d64b90ebe42d9d92f5a0517937a01ebb992db
aegis <aegis@84b32ba4-53c3-423c-be69-77cca6335494>
2002-10-01 00:36:50 +0000

    overwrite existing resources in the build directory if they have already been downloaded
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@1952 84b32ba4-53c3-423c-be69-77cca6335494

--- midworld/get-resources.sh
+++ midworld/get-resources.sh
@@ -3,7 +3,7 @@
 rm -f tmp/resources.zip
 wget --directory-prefix tmp http://isugamedev.sourceforge.net/midworld/data/resources.zip
 mkdir -p build
-(cd build && unzip ../tmp/resources.zip)
+(cd build && unzip -o ../tmp/resources.zip)
 rm -f tmp/resources.zip
 
 # Remove the directory if it's not empty
