b91f459ee61dd768a02b35ebf63246591fb9e9ea
aegis <aegis@84b32ba4-53c3-423c-be69-77cca6335494>
2002-10-02 07:30:13 +0000

    adding documentation to the get-resources.sh script
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@1971 84b32ba4-53c3-423c-be69-77cca6335494

--- midworld/get-resources.sh
+++ midworld/get-resources.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
+# repository:  :pserver:anonymous@pyrallis.aegisknight.org://pyrallis/cvsroot
+# password:    <nothing>
+# module:      midworld-resources
+#
+# If aegis has given you a user account on the CVS server, you may use it
+# instead
+
+
 # checkout the resources from aegis's server
 cvs -d :pserver:anonymous@pyrallis.aegisknight.org://pyrallis/cvsroot \
     checkout -d resources midworld-resources
 
 # install them into the build directory
 cd resources
-./install.sh ../build
+sh install.sh ../build
