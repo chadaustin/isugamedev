28b2746b450f1cb65dd4f3336492674b78bff3f0
aegis <aegis@84b32ba4-53c3-423c-be69-77cca6335494>
2002-10-02 07:22:03 +0000

    get resources from pyrallis.aegisknight.org, not sourceforge
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@1970 84b32ba4-53c3-423c-be69-77cca6335494

--- midworld/get-resources.sh
+++ midworld/get-resources.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 
-rm -f tmp/resources.zip
-wget --directory-prefix tmp http://isugamedev.sourceforge.net/midworld/data/resources.zip
-mkdir -p build
-(cd build && unzip -o ../tmp/resources.zip)
-rm -f tmp/resources.zip
+# checkout the resources from aegis's server
+cvs -d :pserver:anonymous@pyrallis.aegisknight.org://pyrallis/cvsroot \
+    checkout -d resources midworld-resources
 
-# Remove the directory if it's not empty
-FILES=`ls tmp`
-if test "x$FILES" = "x"; then
-   rmdir tmp
-fi
+# install them into the build directory
+cd resources
+./install.sh ../build
