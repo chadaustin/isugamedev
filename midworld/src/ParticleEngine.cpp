3e4a97e08767365f36d40545580f6c8f00a7b6d7
nonchocoboy <nonchocoboy@84b32ba4-53c3-423c-be69-77cca6335494>
2002-10-10 01:52:24 +0000

    Reformatted to the coding style. Some minor bug fixes in here as well.
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@2004 84b32ba4-53c3-423c-be69-77cca6335494

--- midworld/src/ParticleEngine.cpp
+++ midworld/src/ParticleEngine.cpp
@@ -5,197 +5,180 @@
 
 namespace mw
 {
-	ParticleEngine::ParticleEngine(const std::string& filename, const int numParticles, const Camera& gameCamera)
-	{
-		ParticleTexture = new Texture(filename);
-		ParticleCamera = gameCamera;
+   ParticleEngine::ParticleEngine(const std::string& filename,
+                                  const int numParticles,
+                                  const Camera& gameCamera)
+      : mParticleCamera(gameCamera)
+   {
+      mParticleTexture = new Texture(filename);
 
-		for(int i = 0; i < numParticles; i++)
-		{
-			gmtl::Vec3f velocity = randomUnitVector();
-			velocity *= gmtl::Math::rangeRandom(2.0, 5.0);
+      for(int i = 0; i < numParticles; i++)
+      {
+         gmtl::Vec3f velocity = randomUnitVector();
+         velocity *= gmtl::Math::rangeRandom(5.0, 15.0);
 
-			Particle* temp = new Particle(gmtl::Math::rangeRandom(1.0, 2.0));
-			temp->size(0.2);
-			temp->setMass(0.10);
-			temp->setVel(velocity);
-			ParticleGroup.push_back(temp);
-		}
-	}
+         Particle* temp = new Particle(gmtl::Math::rangeRandom(1.0, 2.0));
+         temp->size(0.2);
+         temp->setMass(0.10);
+         temp->setVel(velocity);
+         mParticleGroup.push_back(temp);
+      }
+   }
 
-	ParticleEngine::~ParticleEngine()
-	{
-		delete ParticleTexture;
-	}
+   ParticleEngine::~ParticleEngine()
+   {
+      delete mParticleTexture;
+   }
 
-	void ParticleEngine::setPos(const gmtl::Point3f& thePoint)
-	{
-		point = thePoint;
-	}
+   void ParticleEngine::setPos(const gmtl::Point3f& pos)
+   {
+      mPos = pos;
+   }
 
-	const gmtl::Point3f& ParticleEngine::getPos() const
-	{
-		return point;
-	}
+   const gmtl::Point3f& ParticleEngine::getPos() const
+   {
+      return mPos;
+   }
 
-	bool ParticleEngine::isExpired()
-	{	
-		if(ParticleGroup.size() == 0)
-			return true;
-		else 
-			return false;
-	}
+   bool ParticleEngine::isExpired()
+   {
+      if(mParticleGroup.size() == 0)
+         return true;
+      else
+         return false;
+   }
 
-	void ParticleEngine::draw()
-	{
-		gmtl::Vec3f CameraPos = ParticleCamera.getPos();
-		gmtl::Point3f PartPos;
-		ParticleTexture->bind();
-		
-		//billboardBegin();
-		glDisable(GL_DEPTH_TEST);							
-		//glEnable(GL_BLEND);									
-		//glBlendFunc(GL_SRC_ALPHA,GL_ONE);		
-		for(ParticleList::iterator i=ParticleGroup.begin(); i != ParticleGroup.end(); ++i)
-		{
-			PartPos = (*i)->getPos();
-			billboardBegin(CameraPos, PartPos); 
-			(*i)->draw();
-			billboardEnd();
-		}
-		//glDisable(GL_BLEND);
-		glEnable(GL_DEPTH_TEST);
-		//billboardEnd();
+   void ParticleEngine::draw()
+   {
+      const gmtl::Vec3f& camera_pos = mParticleCamera.getPos();
+      mParticleTexture->bind();
 
-		ParticleTexture->unbind();
-	}
+      glPushAttrib(GL_LIGHTING_BIT | GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+      {
+         glDisable(GL_DEPTH_TEST);
+         glDisable(GL_LIGHTING);
+         glEnable(GL_BLEND);
+         glBlendFunc(GL_SRC_ALPHA, GL_ONE);
 
-	void ParticleEngine::update(float dt)
-	{
-		for(ParticleList::iterator i=ParticleGroup.begin(); i != ParticleGroup.end();)
-		{
-			(*i)->addForce(ParticlePhysics->GRAVITY * (*i)->getMass());
-			(*i)->update(dt);
-			ParticlePhysics->update((*i), dt);
-			(*i)->moveToNextState();
-			if((*i)->isExpired())
-			{	
-				delete (*i);
-				i = ParticleGroup.erase(i);
-			}
-			else
-				i++;
-		}
-	}
+         glColor4f(1,0,0,0.5f);
 
-	/*void ParticleEngine::billboardBegin()
-	{
-		float modelview[16];
-		int i,j;
+         for(ParticleList::iterator i=mParticleGroup.begin(); i != mParticleGroup.end(); ++i)
+         {
+            const gmtl::Point3f& part_pos = (*i)->getPos();
+            billboardBegin(camera_pos, part_pos);
+            (*i)->draw();
+            billboardEnd();
+         }
+      }
+      glPopAttrib();
 
-		// save the current modelview matrix
-		glPushMatrix();
+      mParticleTexture->unbind();
+   }
 
-		// get the current modelview matrix
-		glGetFloatv(GL_MODELVIEW_MATRIX , modelview);
+   void ParticleEngine::update(float dt)
+   {
+      for(ParticleList::iterator i=mParticleGroup.begin(); i != mParticleGroup.end();)
+      {
+         Particle* particle = *i;
 
-		// undo all rotations
-		// beware all scaling is lost as well 
-		for( i=0; i<3; i++ ) 
-		{
-			for( j=0; j<3; j++ ) 
-			{
-				if ( i==j )
-					modelview[i*4+j] = 1.0;
-				else
-					modelview[i*4+j] = 0.0;
-			}
-		}
+         particle->addForce(PhysicsEngine::GRAVITY * particle->getMass());
+         particle->update(dt);
+         mParticlePhysics->update(particle, dt);
+         particle->moveToNextState();
+         if(particle->isExpired())
+         {
+            delete particle;
+            i = mParticleGroup.erase(i);
+         }
+         else if (particle->getPos()[1] <= 0)
+         {
+            delete particle;
+            i = mParticleGroup.erase(i);
+         }
+         else
+         {
+            i++;
+         }
+      }
+   }
 
-		// set the modelview with no rotations
-		glLoadMatrixf(modelview);
-	}*/
+   void ParticleEngine::billboardBegin(const gmtl::Vec3f& cam, const gmtl::Vec3f& objPos)
+   {
+      gmtl::Vec3f lookAt,objToCamProj,upAux, objToCam;
+      float angleCosine;
 
-	void ParticleEngine::billboardBegin(gmtl::Vec3f cam,gmtl::Vec3f objPos)									
-	{
+      glPushMatrix();
 
-		gmtl::Vec3f lookAt,objToCamProj,upAux, objToCam;
-		float angleCosine;
+      // objToCamProj is the vector in world coordinates from the
+      // local origin to the camera projected in the XZ plane
+      objToCamProj[0] = cam[0] - objPos[0];
+      objToCamProj[1] = 0;
+      objToCamProj[2] = cam[2] - objPos[2] ;
 
-		glPushMatrix();
-
-	// objToCamProj is the vector in world coordinates from the 
-	// local origin to the camera projected in the XZ plane
-		objToCamProj[0] = cam[0] - objPos[0];
-		objToCamProj[1] = 0;
-		objToCamProj[2] = cam[2] - objPos[2] ;
-
-	// This is the original lookAt vector for the object 
-	// in world coordinates
-		lookAt[0] = 0;
-		lookAt[1] = 0;
-		lookAt[2] = 1;
+      // This is the original lookAt vector for the object
+      // in world coordinates
+      lookAt[0] = 0;
+      lookAt[1] = 0;
+      lookAt[2] = 1;
 
 
-	// normalize both vectors to get the cosine directly afterwards
-		gmtl::normalize(objToCamProj);
-		//mathsNormalize(objToCamProj);
+      // normalize both vectors to get the cosine directly afterwards
+      gmtl::normalize(objToCamProj);
 
-	// easy fix to determine wether the angle is negative or positive
-	// for positive angles upAux will be a vector pointing in the 
-	// positive y direction, otherwise upAux will point downwards
-	// effectively reversing the rotation.
+      // easy fix to determine wether the angle is negative or positive
+      // for positive angles upAux will be a vector pointing in the
+      // positive y direction, otherwise upAux will point downwards
+      // effectively reversing the rotation.
 
-		upAux = gmtl::cross(lookAt, objToCamProj);
-		//mathsCrossProduct(upAux,lookAt,objToCamProj);
+      upAux = gmtl::cross(lookAt, objToCamProj);
 
-	// compute the angle
-		angleCosine = gmtl::dot(lookAt, objToCamProj);
-		//angleCosine = mathsInnerProduct(lookAt,objToCamProj);
+      // compute the angle
+      angleCosine = gmtl::dot(lookAt, objToCamProj);
 
-	// perform the rotation. The if statement is used for stability reasons
-	// if the lookAt and objToCamProj vectors are too close together then 
-	// |angleCosine| could be bigger than 1 due to lack of precision
-	if ((angleCosine < 0.99990) && (angleCosine > -0.9999))
-		glRotatef(acos(angleCosine)*180/3.14,upAux[0], upAux[1], upAux[2]);	
-	      
-	// so far it is just like the cylindrical billboard. The code for the 
-	// second rotation comes now
-	// The second part tilts the object so that it faces the camera
+      // perform the rotation. The if statement is used for stability reasons
+      // if the lookAt and objToCamProj vectors are too close together then
+      // |angleCosine| could be bigger than 1 due to lack of precision
+      if ((angleCosine < 0.99990) && (angleCosine > -0.9999))
+         glRotatef(acos(angleCosine)*180/3.14,upAux[0], upAux[1], upAux[2]);
 
-	// objToCam is the vector in world coordinates from 
-	// the local origin to the camera
-		objToCam[0] = cam[0] - objPos[0];
-		objToCam[1] = cam[1] - objPos[1];
-		objToCam[2] = cam[2] - objPos[2];
+      // so far it is just like the cylindrical billboard. The code for the
+      // second rotation comes now
+      // The second part tilts the object so that it faces the camera
 
-	// Normalize to get the cosine afterwards
-		gmtl::normalize(objToCam);
-		//mathsNormalize(objToCam);
+      // objToCam is the vector in world coordinates from
+      // the local origin to the camera
+      objToCam[0] = cam[0] - objPos[0];
+      objToCam[1] = cam[1] - objPos[1];
+      objToCam[2] = cam[2] - objPos[2];
 
-	// Compute the angle between objToCamProj and objToCam, 
-	//i.e. compute the required angle for the lookup vector
+      // Normalize to get the cosine afterwards
+      gmtl::normalize(objToCam);
 
-		angleCosine = gmtl::dot(objToCamProj,objToCam);
-		//angleCosine = mathsInnerProduct(objToCamProj,objToCam);
+      // Compute the angle between objToCamProj and objToCam,
+      //i.e. compute the required angle for the lookup vector
+      angleCosine = gmtl::dot(objToCamProj,objToCam);
 
+      // Tilt the object. The test is done to prevent instability
+      // when objToCam and objToCamProj have a very small
+      // angle between them
 
-	// Tilt the object. The test is done to prevent instability 
-	// when objToCam and objToCamProj have a very small
-	// angle between them
+      if ((angleCosine < 0.99990) && (angleCosine > -0.9999))
+      {
+         if (objToCam[1] < 0)
+         {
+            glRotatef(acos(angleCosine)*180/3.14,1,0,0);
+         }
+         else
+         {
+            glRotatef(acos(angleCosine)*180/3.14,-1,0,0);
+         }
+      }
+   }
 
-		if ((angleCosine < 0.99990) && (angleCosine > -0.9999))
-			if (objToCam[1] < 0)
-				glRotatef(acos(angleCosine)*180/3.14,1,0,0);	
-			else
-				glRotatef(acos(angleCosine)*180/3.14,-1,0,0);	
-	      
-	}
-
-	void ParticleEngine::billboardEnd()
-	{
-		// restore the previously 
-		// stored modelview matrix
-		glPopMatrix();
-	}
+   void ParticleEngine::billboardEnd()
+   {
+      // restore the previously
+      // stored modelview matrix
+      glPopMatrix();
+   }
 }
--- midworld/src/ParticleEngine.h
+++ midworld/src/ParticleEngine.h
@@ -1,39 +1,39 @@
 #ifndef MW_PARTICLE_ENGINE_H
 #define MW_PARTICLE_ENGINE_H
 
+#include <list>
+#include <gmtl/Vec.h>
+#include <gmtl/Point.h>
 #include "PhysicsEngine.h"
 #include "Particle.h"
 #include "Texture.h"
 #include "Camera.h"
-#include <gmtl/Vec.h>
-#include <gmtl/Point.h>
-#include <list>
 
 namespace mw
 {
    class ParticleEngine
    {
    public:
-
-	    ParticleEngine(const std::string& filename, const int numParticles, const Camera& gameCamera);
-		~ParticleEngine();
-		void update(float dt);
-		void draw();
-		bool isExpired();
-		void setPos(const gmtl::Point3f& thePoint);
-		const gmtl::Point3f& getPos() const;
+      ParticleEngine(const std::string& filename, const int numParticles, const Camera& gameCamera);
+      ~ParticleEngine();
+      void update(float dt);
+      void draw();
+      bool isExpired();
+      void setPos(const gmtl::Point3f& pos);
+      const gmtl::Point3f& getPos() const;
 
    private:
-		void billboardBegin(gmtl::Vec3f cam,gmtl::Vec3f objPos);
-		void billboardEnd();
+      void billboardBegin(const gmtl::Vec3f& cam, const gmtl::Vec3f& objPos);
+      void billboardEnd();
 
-		gmtl::Point3f point;
-		Texture* ParticleTexture;
+   private:
+      gmtl::Point3f mPos;
+      Texture* mParticleTexture;
 
-		typedef std::list<Particle*> ParticleList;
-		ParticleList ParticleGroup;
-		PhysicsEngine* ParticlePhysics;
-		Camera ParticleCamera;
+      typedef std::list<Particle*> ParticleList;
+      ParticleList mParticleGroup;
+      PhysicsEngine* mParticlePhysics;
+      const Camera& mParticleCamera;
    };
 }
 
