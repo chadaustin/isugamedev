f61ad4868b35961eb138cce717449c0acfbb5132
nonchocoboy <nonchocoboy@84b32ba4-53c3-423c-be69-77cca6335494>
2002-10-30 06:51:18 +0000

    Added copyright header ... where are the comments in this file??!!
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@2093 84b32ba4-53c3-423c-be69-77cca6335494

--- midworld/src/StaticEntity.h
+++ midworld/src/StaticEntity.h
@@ -1,13 +1,41 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil c-basic-offset: 3 -*- */
+// vim:cindent:ts=3:sw=3:et:tw=80:sta:
+/************************************************************* midworld-cpr beg
+ *
+ * midworld - retro post-nuclear mayhem
+ * midworld is (C) Copyright 2002 by
+ *    Chad Austin, Josh Brown, Johnathan Gurley,
+ *    Kevin Meinert, Andres Reinot, Ben Scott
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile: StaticEntity.h,v $
+ * Date modified: $Date: 2002-10-30 06:51:18 $
+ * Version:       $Revision: 1.2 $
+ * -----------------------------------------------------------------
+ *
+ ********************************************************** midworld-cpr-end */
 #ifndef MW_STATIC_ENTITY_H
 #define MW_STATIC_ENTITY_H
 
-
 #include "AbstractEntity.h"
-
 
 namespace mw
 {
-
    class StaticEntity : public AbstractEntity
    {
    public:
@@ -33,8 +61,6 @@ namespace mw
          return false;
       }
    };
-
 }
-
 
 #endif
