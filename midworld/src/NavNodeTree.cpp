769bbbf643cf4dfcc68e3b8896eaea1e4079d3f6
browner10 <browner10@84b32ba4-53c3-423c-be69-77cca6335494>
2002-11-05 10:55:04 +0000

    This addition contains the (very ugly) code that gives us a structure for dealing with navigation nodes for the droids in a logical manner.  The droids will access this object in order to get info about what path to follow to get to the player.  Hopefully this works...
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@2146 84b32ba4-53c3-423c-be69-77cca6335494

---
+++ midworld/src/NavNodeTree.cpp
@@ -0,0 +1,65 @@
+#include "NavNodeTree.h"
+
+
+namespace mw
+{
+   
+
+   bool NavNodeTree::addNode(Node* newNode)
+   {
+      Tree.push_back(newNode);
+      for(int i=0;i<newNode->links.size();i++)
+      {
+         newNode->links[i]->links.push_back(newNode);
+      }
+      return true;
+   }
+
+
+   bool NavNodeTree::addLink(std::string node1, std::string node2)
+   {
+      Node* n1, *n2;
+      for(int i=0;i<Tree.size();i++)
+      {
+         if(Tree[i]->name == node1)
+         {
+            n1 = Tree[i];
+         }
+         else if(Tree[i]->name == node2)
+         {
+            n2 = Tree[i];
+         }
+      }
+      if(n1==NULL || n2==NULL)
+      {
+         return false;
+      }else
+      {
+         
+         n1->links.push_back(n2);
+         n2->links.push_back(n1);
+         return true;
+      }
+   }
+
+
+   // caution use with extreme caution the node defined by name
+   // must have no links currently in it
+   std::vector<Node*>* NavNodeTree::allLinks(std::string node)
+   {
+      Node* n;
+      std::vector<Node*>* nodes = new std::vector<Node*>;
+      for(int i=0;i<Tree.size();i++)
+      {
+         if(Tree[i]->name == node)
+         {
+            n = Tree[i];
+            i=Tree.size();
+         }
+         
+      }
+      return &n->links;
+   }
+
+
+}
---
+++ midworld/src/NavNodeTree.h
@@ -0,0 +1,65 @@
+#ifndef MW_NAV_NODE_TREE_H
+#define MW_NAV_NODE_TREE_H
+
+#include <iostream>
+#include <gmtl/Generate.h>
+#include <vector>
+#include <string>
+
+
+namespace mw
+{
+
+
+   /**
+    * structure to make it easier to deal with Nodes.
+    */
+   struct Node
+   {
+      std::string name;
+      gmtl::Vec3f loc;
+      std::vector<Node*> links;
+   };
+
+
+   /**
+    * class to handle the navigation capabilities of the driods
+    */
+   class NavNodeTree
+   {
+   public:   
+      /**
+       * constructor
+       */
+      NavNodeTree();
+      /**
+       * adds a node to the tree
+       * returns true if successfully added node
+       */
+      bool addNode(Node* newNode);
+      /** 
+       * adds a link between two nodes
+       * returns true if sucessful
+       */
+      bool addLink(std::string node1, std::string node2);
+      /** 
+       * removes a node 
+       * the specified node must not have any links currently 
+       * active.  returns false if there are still links alive.
+       */
+      bool removeNode(std::string name);
+      
+      /**
+       * returns a reference to a vector containing references to all the 
+       * nodes that node has a link to.
+       */
+      std::vector<Node*>* allLinks(std::string node);
+      
+   private:
+      std::vector<Node*> Tree;
+   };
+
+      
+}     
+
+#endif
