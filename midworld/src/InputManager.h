035117071672d01aeef9ac54f59de7f815be9943
nonchocoboy <nonchocoboy@84b32ba4-53c3-423c-be69-77cca6335494>
2002-11-26 01:56:08 +0000

    Coding style fixes and added copyright header.
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@2234 84b32ba4-53c3-423c-be69-77cca6335494

--- midworld/src/InputManager.h
+++ midworld/src/InputManager.h
@@ -1,6 +1,36 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil c-basic-offset: 3 -*- */
+// vim:cindent:ts=3:sw=3:et:tw=80:sta:
+/************************************************************* midworld-cpr beg
+ *
+ * midworld - retro post-nuclear mayhem
+ * midworld is (C) Copyright 2002 by
+ *    Chad Austin, Josh Brown, Johnathan Gurley,
+ *    Kevin Meinert, Andres Reinot, Ben Scott
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile: InputManager.h,v $
+ * Date modified: $Date: 2002-11-26 01:56:08 $
+ * Version:       $Revision: 1.4 $
+ * -----------------------------------------------------------------
+ *
+ ********************************************************** midworld-cpr-end */
 #ifndef MW_INPUT_MANAGER_H
 #define MW_INPUT_MANAGER_H
-
 
 #include <SDL.h>
 #include <string>
@@ -39,6 +69,5 @@ namespace mw
       ButtonMap mButtons;
    };
 }
-
 
 #endif
