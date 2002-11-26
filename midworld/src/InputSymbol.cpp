914190c87df0f262ca5dba3a9a09b9a162d72fe6
nonchocoboy <nonchocoboy@84b32ba4-53c3-423c-be69-77cca6335494>
2002-11-26 07:01:06 +0000

    Refactor:	Case statements are for pussies.
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@2241 84b32ba4-53c3-423c-be69-77cca6335494

--- midworld/src/InputSymbol.cpp
+++ midworld/src/InputSymbol.cpp
@@ -1,17 +1,131 @@
 /* -*- Mode: C++; tab-width: 3; indent-tabs-mode: nil c-basic-offset: 3 -*- */
 // vim:cindent:ts=3:sw=3:et:tw=80:sta:
 
+#include <map>
+#include <stdexcept>
 #include "InputSymbol.h"
 
 namespace mw
 {
+   // Our keymap type from string to SDLkey
+   typedef std::map<std::string, SDLKey> KeyMap;
+
+   namespace
+   {
+      /// Our key mappings
+      static KeyMap gKeys;
+
+      /**
+       * This class inits the list of SDLKey mappings upon construction.
+       */
+      class SDLKeyInit
+      {
+      public:
+         SDLKeyInit()
+         {
+            gKeys["KEY_ESCAPE"]     = SDLK_ESCAPE;
+            gKeys["KEY_F1"]         = SDLK_F1;
+            gKeys["KEY_F2"]         = SDLK_F2;
+            gKeys["KEY_F3"]         = SDLK_F3;
+            gKeys["KEY_F4"]         = SDLK_F4;
+            gKeys["KEY_F5"]         = SDLK_F5;
+            gKeys["KEY_F6"]         = SDLK_F6;
+            gKeys["KEY_F7"]         = SDLK_F7;
+            gKeys["KEY_F8"]         = SDLK_F8;
+            gKeys["KEY_F9"]         = SDLK_F9;
+            gKeys["KEY_F10"]        = SDLK_F10;
+            gKeys["KEY_F11"]        = SDLK_F11;
+            gKeys["KEY_F12"]        = SDLK_F12;
+            gKeys["KEY_F13"]        = SDLK_F13;
+            gKeys["KEY_F14"]        = SDLK_F14;
+            gKeys["KEY_F15"]        = SDLK_F15;
+            gKeys["KEY_TILDE"]      = SDLK_BACKQUOTE;
+            gKeys["KEY_MINUS"]      = SDLK_MINUS;
+            gKeys["KEY_EQUALS"]     = SDLK_EQUALS;
+            gKeys["KEY_BACKSPACE"]  = SDLK_BACKSPACE;
+            gKeys["KEY_TAB"]        = SDLK_TAB;
+            gKeys["KEY_A"]          = SDLK_a;
+            gKeys["KEY_B"]          = SDLK_b;
+            gKeys["KEY_C"]          = SDLK_c;
+            gKeys["KEY_D"]          = SDLK_d;
+            gKeys["KEY_E"]          = SDLK_e;
+            gKeys["KEY_F"]          = SDLK_f;
+            gKeys["KEY_G"]          = SDLK_g;
+            gKeys["KEY_H"]          = SDLK_h;
+            gKeys["KEY_I"]          = SDLK_i;
+            gKeys["KEY_J"]          = SDLK_j;
+            gKeys["KEY_K"]          = SDLK_k;
+            gKeys["KEY_L"]          = SDLK_l;
+            gKeys["KEY_M"]          = SDLK_m;
+            gKeys["KEY_N"]          = SDLK_n;
+            gKeys["KEY_O"]          = SDLK_o;
+            gKeys["KEY_P"]          = SDLK_p;
+            gKeys["KEY_Q"]          = SDLK_q;
+            gKeys["KEY_R"]          = SDLK_r;
+            gKeys["KEY_S"]          = SDLK_s;
+            gKeys["KEY_T"]          = SDLK_t;
+            gKeys["KEY_U"]          = SDLK_u;
+            gKeys["KEY_V"]          = SDLK_v;
+            gKeys["KEY_W"]          = SDLK_w;
+            gKeys["KEY_X"]          = SDLK_x;
+            gKeys["KEY_Y"]          = SDLK_y;
+            gKeys["KEY_Z"]          = SDLK_z;
+            gKeys["KEY_0"]          = SDLK_0;
+            gKeys["KEY_1"]          = SDLK_1;
+            gKeys["KEY_2"]          = SDLK_2;
+            gKeys["KEY_3"]          = SDLK_3;
+            gKeys["KEY_4"]          = SDLK_4;
+            gKeys["KEY_5"]          = SDLK_5;
+            gKeys["KEY_6"]          = SDLK_6;
+            gKeys["KEY_7"]          = SDLK_7;
+            gKeys["KEY_8"]          = SDLK_8;
+            gKeys["KEY_9"]          = SDLK_9;
+            gKeys["KEY_KP0"]        = SDLK_KP0;
+            gKeys["KEY_KP1"]        = SDLK_KP1;
+            gKeys["KEY_KP2"]        = SDLK_KP2;
+            gKeys["KEY_KP3"]        = SDLK_KP3;
+            gKeys["KEY_KP4"]        = SDLK_KP4;
+            gKeys["KEY_KP5"]        = SDLK_KP5;
+            gKeys["KEY_KP6"]        = SDLK_KP6;
+            gKeys["KEY_KP7"]        = SDLK_KP7;
+            gKeys["KEY_KP8"]        = SDLK_KP8;
+            gKeys["KEY_KP9"]        = SDLK_KP9;
+            gKeys["KEY_SHIFT"]      = SDLK_LSHIFT;
+            gKeys["KEY_CTRL"]       = SDLK_LCTRL;
+            gKeys["KEY_ALT"]        = SDLK_LALT;
+            gKeys["KEY_SPACE"]      = SDLK_SPACE;
+            gKeys["KEY_OPENBRACE"]  = SDLK_LEFTBRACKET;
+            gKeys["KEY_CLOSEBRACE"] = SDLK_RIGHTBRACKET;
+            gKeys["KEY_SEMICOLON"]  = SDLK_SEMICOLON;
+            gKeys["KEY_APOSTROPHE"] = SDLK_QUOTE;
+            gKeys["KEY_COMMA"]      = SDLK_COMMA;
+            gKeys["KEY_PERIOD"]     = SDLK_PERIOD;
+            gKeys["KEY_SLASH"]      = SDLK_SLASH;
+            gKeys["KEY_BACKSLASH"]  = SDLK_BACKSLASH;
+            gKeys["KEY_ENTER"]      = SDLK_RETURN;
+            gKeys["KEY_INSERT"]     = SDLK_INSERT;
+            gKeys["KEY_DELETE"]     = SDLK_DELETE;
+            gKeys["KEY_HOME"]       = SDLK_HOME;
+            gKeys["KEY_END"]        = SDLK_END;
+            gKeys["KEY_PAGE_UP"]    = SDLK_PAGEUP;
+            gKeys["KEY_PAGE_DOWN"]  = SDLK_PAGEDOWN;
+            gKeys["KEY_UP"]         = SDLK_UP;
+            gKeys["KEY_DOWN"]       = SDLK_DOWN;
+            gKeys["KEY_LEFT"]       = SDLK_LEFT;
+            gKeys["KEY_RIGHT"]      = SDLK_RIGHT;
+         }
+      };
+
+      static SDLKeyInit initKeys;
+   }
+
    bool isMouseSymbol(const std::string& symbol)
    {
       return (symbol == "MOUSE_LEFT" ||
               symbol == "MOUSE_RIGHT" ||
               symbol == "MOUSE_MIDDLE");
    }
-   
+
    Uint8 stringToSDLButton(const std::string& symbol)
    {
       if (symbol == "MOUSE_LEFT")
@@ -34,369 +148,28 @@ namespace mw
 
    SDLKey stringToSDLKey(const std::string& name)
    {
-      if (name == "KEY_ESCAPE")
+      KeyMap::iterator itr = gKeys.find(name);
+      if (itr != gKeys.end())
       {
-         return SDLK_ESCAPE;
-      }
-      else if (name == "KEY_F1")
-      {
-         return SDLK_F1;
-      }
-      else if (name == "KEY_F2")
-      {
-         return SDLK_F2;
-      }
-      else if (name == "KEY_F3")
-      {
-         return SDLK_F3;
-      }
-      else if (name == "KEY_F4")
-      {
-         return SDLK_F4;
-      }
-      else if (name == "KEY_F5")
-      {
-         return SDLK_F5;
-      }
-      else if (name == "KEY_F6")
-      {
-         return SDLK_F6;
-      }
-      else if (name == "KEY_F7")
-      {
-         return SDLK_F7;
-      }
-      else if (name == "KEY_F8")
-      {
-         return SDLK_F8;
-      }
-      else if (name == "KEY_F9")
-      {
-         return SDLK_F9;
-      }
-      else if (name == "KEY_F10")
-      {
-         return SDLK_F10;
-      }
-      else if (name == "KEY_F11")
-      {
-         return SDLK_F11;
-      }
-      else if (name == "KEY_F12")
-      {
-         return SDLK_F12;
-      }
-      else if (name == "KEY_F13")
-      {
-         return SDLK_F13;
-      }
-      else if (name == "KEY_F14")
-      {
-         return SDLK_F14;
-      }
-      else if (name == "KEY_F15")
-      {
-         return SDLK_F15;
-      }
-      else if (name == "KEY_TILDE")
-      {
-         return SDLK_BACKQUOTE;
-      }
-      else if (name == "KEY_MINUS")
-      {
-         return SDLK_MINUS;
-      }
-      else if (name == "KEY_EQUALS")
-      {
-         return SDLK_EQUALS;
-      }
-      else if (name == "KEY_BACKSPACE")
-      {
-         return SDLK_BACKSPACE;
-      }
-      else if (name == "KEY_TAB")
-      {
-         return SDLK_TAB;
-      }
-      else if (name == "KEY_A")
-      {
-         return SDLK_a;
-      }
-      else if (name == "KEY_B")
-      {
-         return SDLK_b;
-      }
-      else if (name == "KEY_C")
-      {
-         return SDLK_c;
-      }
-      else if (name == "KEY_D")
-      {
-         return SDLK_d;
-      }
-      else if (name == "KEY_E")
-      {
-         return SDLK_e;
-      }
-      else if (name == "KEY_F")
-      {
-         return SDLK_f;
-      }
-      else if (name == "KEY_G")
-      {
-         return SDLK_g;
-      }
-      else if (name == "KEY_H")
-      { 
-         return SDLK_h;
-      }
-      else if (name == "KEY_I")
-      {
-         return SDLK_i;
-      }
-      else if (name == "KEY_J")
-      {
-         return SDLK_j;
-      }
-      else if (name == "KEY_K")
-      {
-         return SDLK_k;
-      }
-      else if (name == "KEY_L")
-      {
-         return SDLK_l;
-      }
-      else if (name == "KEY_M")
-      {
-         return SDLK_m;
-      }
-      else if (name == "KEY_N")
-      {
-         return SDLK_n;
-      }
-      else if (name == "KEY_O")
-      {
-         return SDLK_o;
-      }
-      else if (name == "KEY_P")
-      {
-         return SDLK_p;
-      }
-      else if (name == "KEY_Q")
-      {
-         return SDLK_q;
-      }
-      else if (name == "KEY_R")
-      {
-         return SDLK_r;
-      }
-      else if (name == "KEY_S")
-      {
-         return SDLK_s;
-      }
-      else if (name == "KEY_T")
-      {
-         return SDLK_t;
-      }
-      else if (name == "KEY_U")
-      {
-         return SDLK_u;
-      }
-      else if (name == "KEY_V")
-      {
-         return SDLK_v;
-      }
-      else if (name == "KEY_W")
-      {
-         return SDLK_w;
-      }
-      else if (name == "KEY_X")
-      {
-         return SDLK_x;
-      }
-      else if (name == "KEY_Y")
-      {
-         return SDLK_y;
-      }
-      else if (name == "KEY_Z")
-      {
-         return SDLK_z;
-      }
-      else if (name == "KEY_0")
-      {
-         return SDLK_0;
-      }
-      else if (name == "KEY_1")
-      {
-         return SDLK_1;
-      }
-      else if (name == "KEY_2")
-      {
-         return SDLK_2;
-      }
-      else if (name == "KEY_3")
-      {
-         return SDLK_3;
-      }
-      else if (name == "KEY_4")
-      {
-         return SDLK_4;
-      }
-      else if (name == "KEY_5")
-      {
-         return SDLK_5;
-      }
-      else if (name == "KEY_6")
-      {
-         return SDLK_6;
-      }
-      else if (name == "KEY_7")
-      {
-         return SDLK_7;
-      }
-      else if (name == "KEY_8")
-      {
-         return SDLK_8;
-      }
-      else if (name == "KEY_9")
-      {
-         return SDLK_9;
-      }
-      else if (name == "KEY_KP0")
-      {
-         return SDLK_KP0;
-      }
-      else if (name == "KEY_KP1")
-      {
-         return SDLK_KP1;
-      }
-      else if (name == "KEY_KP2")
-      {
-         return SDLK_KP2;
-      }
-      else if (name == "KEY_KP3")
-      {
-         return SDLK_KP3;
-      }
-      else if (name == "KEY_KP4")
-      {
-         return SDLK_KP4;
-      }
-      else if (name == "KEY_KP5")
-      {
-         return SDLK_KP5;
-      }
-      else if (name == "KEY_KP6")
-      {
-         return SDLK_KP6;
-      }
-      else if (name == "KEY_KP7")
-      {
-         return SDLK_KP7;
-      }
-      else if (name == "KEY_KP8")
-      {
-         return SDLK_KP8;
-      }
-      else if (name == "KEY_KP9")
-      {
-         return SDLK_KP9;
-      }
-      else if (name == "KEY_SHIFT")
-      {
-         return SDLK_LSHIFT;
-      }
-      else if (name == "KEY_CTRL")
-      {
-         return SDLK_LCTRL;
-      }
-      else if (name == "KEY_ALT")
-      {
-         return SDLK_LALT;
-      }
-      else if (name == "KEY_SPACE")
-      {
-         return SDLK_SPACE;
-      }
-      else if (name == "KEY_OPENBRACE")
-      {
-         return SDLK_LEFTBRACKET;
-      }
-      else if (name == "KEY_CLOSEBRACE")
-      {
-         return SDLK_RIGHTBRACKET;
-      }
-      else if (name == "KEY_SEMICOLON")
-      {
-         return SDLK_SEMICOLON;
-      }
-      else if (name == "KEY_APOSTROPHE")
-      {
-         return SDLK_QUOTE;
-      }
-      else if (name == "KEY_COMMA")
-      {
-         return SDLK_COMMA;
-      }
-      else if (name == "KEY_PERIOD")
-      {
-         return SDLK_PERIOD;
-      }
-      else if (name == "KEY_SLASH")
-      {
-         return SDLK_SLASH;
-      }
-      else if (name == "KEY_BACKSLASH")
-      {
-         return SDLK_BACKSLASH;
-      }
-      else if (name == "KEY_ENTER")
-      {
-         return SDLK_RETURN;
-      }
-      else if (name == "KEY_INSERT")
-      {
-         return SDLK_INSERT;
-      }
-      else if (name == "KEY_DELETE")
-      {
-         return SDLK_DELETE;
-      }
-      else if (name == "KEY_HOME")
-      {
-         return SDLK_HOME;
-      }
-      else if (name == "KEY_END")
-      {
-         return SDLK_END;
-      }
-      else if (name == "KEY_PAGE_UP")
-      {
-         return SDLK_PAGEUP;
-      }
-      else if (name == "KEY_PAGE_DOWN")
-      {
-         return SDLK_PAGEDOWN;
-      }
-      else if (name == "KEY_UP")
-      {
-         return SDLK_UP;
-      }
-      else if (name == "KEY_DOWN")
-      {
-         return SDLK_DOWN;
-      }
-      else if (name == "KEY_LEFT")
-      {
-         return SDLK_LEFT;
-      }
-      else if (name == "KEY_RIGHT")
-      {
-         return SDLK_RIGHT;
+         return itr->second;
       }
       else
       {
          return SDLK_UNKNOWN;
       }
    }
-}//end of namespace mw
+
+   const std::string& SDLKeyToString(const SDLKey& key)
+   {
+      for (KeyMap::iterator itr = gKeys.begin(); itr != gKeys.end(); ++itr)
+      {
+         if (itr->second == key)
+         {
+            return itr->first;
+         }
+      }
+
+      // Couldn't find a match
+      throw std::runtime_error("Couldn't find a name for the given SDL key");
+   }
+}
