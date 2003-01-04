4232d11ae52fabfc933adbade5d392a8eebe97aa
nonchocoboy <nonchocoboy@84b32ba4-53c3-423c-be69-77cca6335494>
2003-01-04 06:20:46 +0000

    Added copy header.
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@2316 84b32ba4-53c3-423c-be69-77cca6335494

--- lib/phui/phui/FlowLayout.h
+++ lib/phui/phui/FlowLayout.h
@@ -1,8 +1,36 @@
 /* -*- Mode: C++; tab-width: 3; indent-tabs-mode: nil c-basic-offset: 3 -*- */
 // vim:cindent:ts=3:sw=3:et:tw=80:sta:
-
-#ifndef PHUI_FLOWLAYOUT_H
-#define PHUI_FLOWLAYOUT_H
+/***************************************************************** phui-cpr beg
+ *
+ * phui - flexible user interface subsystem
+ * phui is (C) Copyright 2002 by
+ *      Chad Austin, Josh Carlson, Johnathan Gurley,
+ *      Ben Scott, Levi Van Oort
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile: FlowLayout.h,v $
+ * Date modified: $Date: 2003-01-04 06:19:33 $
+ * Version:       $Revision: 1.2 $
+ * -----------------------------------------------------------------
+ *
+ ************************************************************** phui-cpr-end */
+#ifndef PHUI_FLOW_LAYOUT_H
+#define PHUI_FLOW_LAYOUT_H
 
 #include "LayoutConstraint.h"
 #include "LayoutManager.h"
@@ -15,20 +43,20 @@ namespace phui
    class FlowLayout : public LayoutManager
    {
    public:
-      
       /**
        * Constructor
        *
        * @param   container   the widget container to manage
        * @param   constraint  the layout contstraint to follow
        */
-      FlowLayout(WidgetContainer* container, LayoutConstraint* constraint) 
-         : LayoutManager(container, constraint), mCurrentWidth(0), \
-           mCurrentMaxWidth(0), mCurrentHeight(0), mCurrentMaxHeight(0)
-      {
-         
-      }
-      
+      FlowLayout(WidgetContainer* container, LayoutConstraint* constraint)
+         : LayoutManager(container, constraint)
+         , mCurrentWidth(0)
+         , mCurrentMaxWidth(0)
+         , mCurrentHeight(0)
+         , mCurrentMaxHeight(0)
+      {}
+
       /**
        * Adds a widget's specifications to this layout manager
        *
@@ -36,7 +64,7 @@ namespace phui
        * @param   size        the size of the widget to be added
        */
       virtual void add(const Point& point, const Size& size);
-      
+
       /**
        * Removes a widget's specifications from this layout manager
        *
@@ -49,16 +77,16 @@ namespace phui
       }
 
    private:
-      
-      ///Current Width 
+
+      ///Current Width
       int mCurrentWidth;
-      ///Current Maximum Width 
+      ///Current Maximum Width
       int mCurrentMaxWidth;
-      ///Current Height 
+      ///Current Height
       int mCurrentHeight;
       ///Current Maximum Height
       int mCurrentMaxHeight;
-      
+
    };
 }
 
--- lib/phui/phui/FlowVerticalConstraint.h
+++ lib/phui/phui/FlowVerticalConstraint.h
@@ -1,15 +1,43 @@
 /* -*- Mode: C++; tab-width: 3; indent-tabs-mode: nil c-basic-offset: 3 -*- */
 // vim:cindent:ts=3:sw=3:et:tw=80:sta:
-
-#ifndef PHUI_FLOWVERTICALCONSTRAINT_H
-#define PHUI_FLOWVERTICALCONSTRAINT_H
+/***************************************************************** phui-cpr beg
+ *
+ * phui - flexible user interface subsystem
+ * phui is (C) Copyright 2002 by
+ *      Chad Austin, Josh Carlson, Johnathan Gurley,
+ *      Ben Scott, Levi Van Oort
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile: FlowVerticalConstraint.h,v $
+ * Date modified: $Date: 2003-01-04 06:20:46 $
+ * Version:       $Revision: 1.2 $
+ * -----------------------------------------------------------------
+ *
+ ************************************************************** phui-cpr-end */
+#ifndef PHUI_FLOW_VERTICAL_CONSTRAINT_H
+#define PHUI_FLOW_VERTICAL_CONSTRAINT_H
 
 #include "LayoutConstraint.h"
 
 namespace phui
 {
    /**
-    * Layout constraint that makes a flow layout flow vertically 
+    * Layout constraint that makes a flow layout flow vertically.
     */
    class FlowVerticalConstraint : public LayoutConstraint
    {
