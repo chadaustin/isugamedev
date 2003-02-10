7dd0978c9294d38f3943393a04b9c2c15cd39267
subatomic <subatomic@84b32ba4-53c3-423c-be69-77cca6335494>
2003-02-10 05:09:02 +0000

    DeviceHandle is funny.  it assumes that you don't want to configure your device after you create it.
    Basically I found a problem where if you configure joystick (starts with
    0 inputs), through DeviceHandle->getDevice()->, then the GameInput will
    not call the refreshBindings function (and can't because it would be hard
    to notify it with the current architecture).
    
    to fix, I added another constructor to take a preconfigured device
    pointer (basically allow someone else to create and set up the device).
    and I added a refresh() function to remove/re-add the device (forcing
    GameInput to refresh bindings).
    
    a little hacky, but it works.  it still requires that the user be
    diligent paying attention that they need to refresh the devicehandle...
    :(  not something we want to require, but luckily the only client of this
    code is the GameKernel drivers so we don't have to worry too much about
    it for the users.
    
    
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@2414 84b32ba4-53c3-423c-be69-77cca6335494

--- lib/gamekernel/gk/IGameInput.h
+++ lib/gamekernel/gk/IGameInput.h
@@ -24,8 +24,8 @@
 //
 // -----------------------------------------------------------------
 // File:          $RCSfile: IGameInput.h,v $
-// Date modified: $Date: 2003-02-09 07:43:03 $
-// Version:       $Revision: 1.4 $
+// Date modified: $Date: 2003-02-10 05:09:02 $
+// Version:       $Revision: 1.5 $
 // -----------------------------------------------------------------
 //
 ////////////////// <GK heading END do not edit this line> ///////////////////
@@ -154,6 +154,9 @@ public:
  * Device wrapper that provides automatic addition and removal of the contained
  * device with the input manager.
  *
+ * WARNING: do not change the number of inputs a device has after DeviceHandle has been created.
+ *          if you need to, then you must call refresh() to force the GameInput manager to see the new inputs.
+ *
  * @param Type    the type of device to manage
  */
 template< class Type >
@@ -172,6 +175,22 @@ public:
       : mName( name ), mKernel( kernel )
    {
       mDevice = new Type();
+      IGameInput* input = mKernel->getInput();
+      input->addDevice( mDevice, mName );
+   }
+
+   /**
+    * Creates and instance of the device type and adds it to the input manager.
+    *
+    * @param name    the name of the device
+    * @param kernel  the kernel with which to attach this device to
+    * @param dev     the device, device handle assumes ownership of the pointer, and will delete memory for you.
+    *
+    * @pre kernel != NULL
+    */
+   DeviceHandle( const std::string& name, IGameKernel* kernel, Type* dev )
+      : mName( name ), mKernel( kernel ), mDevice( dev )
+   {
       IGameInput* input = mKernel->getInput();
       input->addDevice( mDevice, mName );
    }
@@ -214,6 +233,17 @@ public:
    const std::string& getName() const
    {
       return mName;
+   }
+
+   /**
+    *  remove and re-add the device.
+    *  call this when you reconfigure the device inputs 
+    *  notifys the GameInput manager to remap bindings.
+    */
+   void refresh()
+   {
+      mKernel->getInput()->removeDevice( mName );
+      mKernel->getInput()->addDevice( mDevice, mName );
    }
 
 private:
