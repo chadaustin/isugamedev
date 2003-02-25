bd8c472582a81e4a84c11441c89c4b659cab3af6
dshipton <dshipton@84b32ba4-53c3-423c-be69-77cca6335494>
2003-02-25 05:38:49 +0000

    Chase cam added :)
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@2470 84b32ba4-53c3-423c-be69-77cca6335494

---
+++ lib/siren/src/siren/Camera.cpp
@@ -0,0 +1,175 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil c-basic-offset: 3 -*- */
+// vim:cindent:ts=3:sw=3:et:tw=80:sta:
+
+#include <gmtl/Generate.h>
+#include <gmtl/QuatOps.h>
+#include <SDL_opengl.h>
+#include "Camera.h"
+
+namespace siren
+{
+   Camera::Camera()
+      : mTargetPos()
+      , mLastTargetPos()
+      , mTargetRot()
+      , mLastTargetRot()
+      , mFollowDistance(10)
+      , mFollowDistanceVel(0)
+      , mMaxFollowDistance(15)
+      , mMinFollowDistance(5)
+      , mPitch(gmtl::Math::deg2Rad(40.0f))
+      , mPitchVel(0)
+      , mMaxPitch(gmtl::Math::deg2Rad(75.0f))
+      , mMinPitch(gmtl::Math::deg2Rad(15.0f))
+      , mTransform()
+   {}
+
+   void
+   Camera::setTarget(const gmtl::Point3f& pos, const gmtl::Quatf& rot)
+   {
+      mTargetPos = pos;
+      mTargetRot = rot;
+   }
+
+   void
+   Camera::update(float dt)
+   {
+      // Calculate our state updates assuming that our dt is twice as large so
+      // that we get the impression that the camera moves half the distance.
+      float camera_time = 2.0 * dt;
+
+      // Calculate the new rotation
+      gmtl::Quatf new_rot;
+      gmtl::slerp(new_rot, camera_time, mLastTargetRot, mTargetRot);
+      mLastTargetRot = new_rot;
+
+      // Calculate the new position
+      gmtl::Point3f new_pos;
+      gmtl::lerp(new_pos, camera_time, mLastTargetPos, mTargetPos);
+      mLastTargetPos = new_pos;
+
+      // Calculate the matrix for the target location
+      gmtl::CoordVec3AxisAnglef target_coord(
+            new_pos,
+            gmtl::make<gmtl::AxisAnglef>(new_rot));
+
+      mTransform = gmtl::make<gmtl::Matrix44f>(target_coord);
+
+      // Apply the camera's offset information
+      mTransform *= gmtl::makeRot<gmtl::Matrix44f>(gmtl::AxisAnglef(-mPitch, gmtl::Vec3f(1,0,0)));
+      mTransform *= gmtl::makeTrans<gmtl::Matrix44f>(gmtl::Vec3f(0,0,mFollowDistance));
+
+//      gmtl::makeTrans<gmtl::Matrix44f>(target_mat, new_pos);
+//      target_mat *= gmtl::makeRot<gmtl::Matrix44f>(
+
+      // Update yaw, pitch, and follow distance by their velocities
+      mPitch            += mPitchVel * dt;
+      mFollowDistance   += mFollowDistanceVel * dt;
+      
+      // Check bounds on follow distance and pitch
+      if (mFollowDistance > mMaxFollowDistance)
+      {
+         mFollowDistance = mMaxFollowDistance;
+      }
+      if (mFollowDistance < mMinFollowDistance)
+      {
+         mFollowDistance = mMinFollowDistance;
+      }
+      if (mPitch > mMaxPitch)
+      {
+         mPitch = mMaxPitch;
+      }  
+      if (mPitch < mMinPitch)
+      {
+         mPitch = mMinPitch;
+      } 
+   }
+
+   void
+   Camera::draw()
+   {
+      gmtl::Matrix44f cam_inv = gmtl::makeInverse(mTransform);
+      glMultMatrixf(cam_inv.getData());
+
+//      glRotatef( 90, 1,0,0 );
+//      glTranslatef( mPos[0], mPos[1] - mDistance, mPos[2] );
+   }
+
+   void
+   Camera::setFollowDistance(float distance)
+   {
+      if (distance > mMaxFollowDistance)
+      {
+         mFollowDistance = mMaxFollowDistance;
+      }
+      else if (distance < mMinFollowDistance)
+      {
+         mFollowDistance = mMinFollowDistance;
+      }
+      else
+      {
+         mFollowDistance = distance;
+      }
+   }
+
+   void
+   Camera::setMaxFollowDistance(float max)
+   {
+      mMaxFollowDistance = max;
+   }
+   
+   void
+   Camera::setMinFollowDistance(float min)
+   {
+      mMinFollowDistance = min;
+   }
+
+   void
+   Camera::setFollowDistanceVel(float vel)
+   {
+      mFollowDistanceVel = vel;
+   }
+
+   float
+   Camera::getPitch() const
+   {
+      return mPitch;
+   }
+
+   void
+   Camera::setPitch(float angle)
+   {
+      if (angle > mMaxPitch)
+      {
+         mPitch = mMaxPitch;
+      }
+      else
+      {     
+         mPitch = angle;
+      }
+   }
+
+   void
+   Camera::setMaxPitch(float max)
+   {
+      mMaxPitch = max;
+   }
+  
+   void
+   Camera::setMinPitch(float min)
+   {
+      mMinPitch = min;
+   }
+
+   void
+   Camera::setPitchVel(float vel)
+   {
+      mPitchVel = vel;
+   }
+
+   gmtl::Vec3f
+   Camera::getPos() const
+   {
+      return gmtl::makeTrans<gmtl::Vec3f>(mTransform);
+   }
+}
---
+++ lib/siren/src/siren/Camera.h
@@ -0,0 +1,144 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil c-basic-offset: 3 -*- */
+// vim:cindent:ts=3:sw=3:et:tw=80:sta:
+
+#ifndef MW_CAMERA_H
+#define MW_CAMERA_H
+
+#include <gmtl/Matrix.h>
+#include <gmtl/Point.h>
+#include <gmtl/Quat.h>
+
+namespace siren
+{
+   /**
+    * This camera will "chase" a target position/orientation.
+    */
+   class Camera
+   {
+   public:
+      /**
+       * Creates a new Camera looking at the origin from 20 units up the y-axis.
+       */
+      Camera();
+
+      /**
+       * Sets the target position and rotation for the camera. The camera will
+       * attempt to move to the target position and orient itself to the target
+       * rotation. The camera will also take into account its added follow
+       * distance and yaw/pitch angles.
+       *
+       * @param pos     the target position
+       * @param rot     the target rotation
+       */
+      void setTarget(const gmtl::Point3f& pos, const gmtl::Quatf& rot);
+
+      /**
+       * Updates the state of this camera based on the amount of time that has
+       * passed since the last update. This camera will position itself halfway
+       * to the destination position and rotation on each call to update based
+       * on its current position and rotation.
+       *
+       * @param dt      the amount of time that passed in seconds
+       */
+      void update(float dt);
+
+      void draw();
+
+      /**
+       * Sets the distance by which this camera should follow the target
+       * position.
+       *
+       * @param distance   the follow distance
+       */
+      void setFollowDistance(float distance);
+      
+      /**
+       * Sets the maximum following distance of the camera
+       *
+       * @param max        the maximum following distance
+       */
+      void setMaxFollowDistance(float max);
+
+      /**
+       * Sets the minimum following distance of the camera
+       *
+       * @param min        the minimum following distance
+       */
+      void setMinFollowDistance(float min);
+
+      /**
+       * Sets the velocity by which the follow distance should change.
+       *
+       * @param vel        the velocity of the follow distance
+       */
+      void setFollowDistanceVel(float vel);
+
+
+      /**
+       * Gets the pitch angle this camera adds to the target rotation.
+       *
+       * @return  the pitch angle in radians
+       */
+      float getPitch() const;
+
+      /**
+       * Sets the pitch angle this camera will add to the target rotation.
+       *
+       * @param angle      the pitch angle in radians
+       */
+      void setPitch(float angle);
+      
+      /**
+       * Sets the maximum pitch angle this camera will rotate to.
+       *
+       * @param max        the maximum pitch angle in radians
+       */
+      void setMaxPitch(float max);
+      
+      /**
+       * Sets the minimum pitch angle this camera will rotate to.
+       *
+       * @param min        the minimum pitch angle in radians
+       */
+      void setMinPitch(float min);
+
+      /**
+       * Sets the angular velocity by which the pitch angle should change.
+       *
+       * @param vel        the angular velocity of the pitch angle
+       */
+      void setPitchVel(float vel);
+
+      /**
+       * Gets the position of this camera in world coordinates.
+       */
+      gmtl::Vec3f getPos() const;
+
+   private:
+      gmtl::Point3f mTargetPos;
+      gmtl::Point3f mLastTargetPos;
+
+      gmtl::Quatf mTargetRot;
+      gmtl::Quatf mLastTargetRot;
+
+      // The distance by which the camera is following the target.
+      float mFollowDistance;
+      float mFollowDistanceVel;
+      // The bounds on the camera's following distance
+      float mMaxFollowDistance;
+      float mMinFollowDistance;
+
+      float mPitch;
+      float mPitchVel;
+
+      // The bounds on the camera's pitch
+      float mMaxPitch;
+      float mMinPitch;
+
+      // Cache of the current transform for the location of the camera.
+      gmtl::Matrix44f mTransform;
+   };
+
+}
+
+#endif
