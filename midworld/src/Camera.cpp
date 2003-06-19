39143d6f52ddc57848109ec0be059ca5ef21c087
nonchocoboy <nonchocoboy@84b32ba4-53c3-423c-be69-77cca6335494>
2003-06-19 03:19:45 +0000

    Don't modify the camera transform member when trying to get its inverse.
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@2582 84b32ba4-53c3-423c-be69-77cca6335494

--- midworld/src/Camera.cpp
+++ midworld/src/Camera.cpp
@@ -24,8 +24,8 @@
  *
  * -----------------------------------------------------------------
  * File:          $RCSfile: Camera.cpp,v $
- * Date modified: $Date: 2003-06-16 08:14:56 $
- * Version:       $Revision: 1.11 $
+ * Date modified: $Date: 2003-06-19 03:19:45 $
+ * Version:       $Revision: 1.12 $
  * -----------------------------------------------------------------
  *
  ********************************************************** midworld-cpr-end */
@@ -116,7 +116,7 @@ namespace mw
    void
    Camera::draw()
    {
-      gmtl::Matrix44f cam_inv = gmtl::invert(mTransform);
+      gmtl::Matrix44f cam_inv = gmtl::makeInvert(mTransform);
       glMultMatrixf(cam_inv.getData());
 
 //      glRotatef( 90, 1,0,0 );
