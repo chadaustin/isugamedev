2a9067a23c7d0ab7ae40842fbac1b4087bcff727
daren <daren@84b32ba4-53c3-423c-be69-77cca6335494>
2003-08-18 06:06:43 +0000

    - These are unnecessary.
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@2591 84b32ba4-53c3-423c-be69-77cca6335494

--- darenland/metropolis/cvsme.py
+++
@@ -1,9 +0,0 @@
-import os, sys, shutil
-pj = os.path.join
-
-def CVSme(dir):
-	for x in os.listdir(dir):
-		os.system('cvs add ' + pj(dir, x))
-		if os.path.isdir(pj(dir, x)) and x != 'CVS':
-			CVSme(pj(dir, x))
-CVSme('./')
