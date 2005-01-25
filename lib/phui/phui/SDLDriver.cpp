615261ba59a97342564369b4023a7fee49c3f178
daren <daren@84b32ba4-53c3-423c-be69-77cca6335494>
2005-01-25 01:42:42 +0000

    Copyright update
    git-svn-id: file:///desktop/SourceForge%20Backup/isugamedev/svnrepos/trunk@2624 84b32ba4-53c3-423c-be69-77cca6335494

--- lib/phui/phui/SDLDriver.cpp
+++ lib/phui/phui/SDLDriver.cpp
@@ -1,3 +1,34 @@
+/* -*- Mode: C++; tab-width: 3; indent-tabs-mode: nil c-basic-offset: 3 -*- */
+// vim:cindent:ts=3:sw=3:et:tw=80:sta:
+/***************************************************************** phui-cpr beg
+ *
+ * phui - flexible user interface subsystem
+ * phui is (C) Copyright 2002 by
+ *      Chad Austin, Josh Carlson, Johnathan Gurley,
+ *      Ben Scott, Levi Van Oort
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile: SDLDriver.cpp,v $
+ * Date modified: $Date: 2005-01-25 01:42:05 $
+ * Version:       $Revision: 1.3 $
+ * -----------------------------------------------------------------
+ *
+ ************************************************************** phui-cpr-end */
 #include <phui/SDLDriver.h>
 #include <phui/Debug.h>
 #include <phui/DriverManager.h>
